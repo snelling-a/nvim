73eead6166eaba63f2125224835f357f246cbc8d
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-01-22 21:58:00 +0100

    Initial commit
    after doing some reseearch it was becoming clear that this will be a
    bigger poject than a vim configuration

---
+++ lua/user/init.lua
@@ -0,0 +1,34 @@
+require("user.packer")
+require("user.remap")
+require("user.set")
+
+local augroup = vim.api.nvim_create_augroup
+local StripWhitespaceGroup = augroup("StripWhitespaceGroup", {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({ "BufWritePre" }, {
+    group = StripWhitespaceGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
