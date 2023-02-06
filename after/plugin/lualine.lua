e67278d35e6d46566d2f7055b8233041c1423b5e
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-02-06 12:00:50 +0100

    Lualine revamp
    - replace Bufferline with Lualine tabline
    - added Noice components
    - filetype names
    - symbols for file status

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,50 +1,131 @@
-require('lualine').setup {
-    options = {
-        disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-        }
-    },
-    sections = {
-        lualine_a = { 'mode' },
-        lualine_b = {
-            { 'branch' },
-            {
-                'diff',
-                symbols = { added = "  ", modified = "柳", removed = " " }, -- changes diff symbols
-            },
-            { 'diagnostics' }
-        },
-        lualine_c = {},
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-        lualine_y = {},
-        lualine_z = {}
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {
-        "fugitive",
-        "fzf",
-        "man",
-        "nvim-tree",
-        "quickfix",
-        "symbols-outline",
-    }
+local ok, lualine = pcall(require, "lualine")
+if not ok or vim.g.started_by_firenvim then
+	return nil
+end
+
+local base09 = "#dc9656"
+
+local modified = "柳"
+
+local lsp_progress = {
+	"lsp_progress",
+	spinner_symbols = {
+		"▰▱▱▱▱▱▱",
+		"▰▰▱▱▱▱▱",
+		"▰▰▰▱▱▱▱",
+		"▰▰▰▰▱▱▱",
+		"▰▰▰▰▰▱▱",
+		"▰▰▰▰▰▰▱",
+		"▰▰▰▰▰▰▰",
+	},
 }
+
+local noice_command = {
+	require("noice").api.status.command.get,
+	cond = require("noice").api.status.command.has,
+	color = { fg = base09 },
+}
+
+local noice_searchcount = {
+	require("noice").api.status.search.get,
+	cond = require("noice").api.status.search.has,
+	color = { fg = base09 },
+}
+
+local filetype_names = {
+	TelescopePrompt = "Telescope",
+	packer = "Packer",
+}
+
+local filename = {
+	"filename",
+	path = 1,
+	symbols = {
+		modified = modified,
+		newfile = "",
+		readonly = "",
+		unnamed = "",
+	},
+}
+
+lualine.setup({
+	options = {
+		disabled_filetypes = {
+			statusline = { "DiffviewFiles", "NvimTree", "packer", "undotree" },
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		},
+	},
+	sections = {
+		lualine_a = {
+			"mode",
+		},
+		lualine_b = {
+			filename,
+			{ "diff", symbols = { added = "  ", modified = modified, removed = " " } },
+			{ "diagnostics" },
+		},
+		lualine_c = { lsp_progress },
+		lualine_x = {
+			noice_searchcount,
+			noice_command,
+		},
+		lualine_y = {
+			{
+				"filetype",
+				icon_only = true,
+			},
+			"progress",
+			"location",
+		},
+		lualine_z = { "branch" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { filename },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {
+		lualine_a = {
+			{ "buffers", filetype_names },
+		},
+		lualine_b = {
+			{
+				"macro-recording",
+				fmt = function()
+					local recording_register = vim.fn.reg_recording()
+					if recording_register == "" then
+						return ""
+					else
+						return "recording @" .. recording_register
+					end
+				end,
+				color = { fg = "orange" },
+			},
+		},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = { "tabs" },
+	},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {
+		"fugitive",
+		"fzf",
+		"man",
+		"nvim-tree",
+		"quickfix",
+		"symbols-outline",
+		"toggleterm",
+	},
+})
