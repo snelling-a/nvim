42d4180369c0f3b533e8e773efcbc706ea493f69
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-03-21 12:15:56 +0100

    Impatient

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -39,6 +39,8 @@ packer.init({
 return packer.startup(function(use)
 	use({ "wbthomason/packer.nvim" })
 
+	use({ "lewis6991/impatient.nvim" })
+
 	-- [[ UI]]
 	use({
 		"eandrju/cellular-automaton.nvim",
