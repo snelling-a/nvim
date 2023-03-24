cc11caa106a5baa741fde9033455183554b9c2d0
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-03-24 14:22:12 +0100

    Markdown improvements
    - alex https://github.com/get-alex/alex
    - add additional highlighting through treesitter
    - wrap/conceal in markdown files

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -19,7 +19,7 @@ require("nvim-treesitter.configs").setup({
 	},
 	sync_install = false,
 	auto_install = true,
-	highlight = { enable = true, additional_vim_regex_highlighting = false },
+	highlight = { enable = true, additional_vim_regex_highlighting = { "markdown" } },
 	textobjects = {
 		select = {
 			enable = true,
