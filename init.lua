0edaff9a74fe53cc701784303f6edd8be406d1ed
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-04-04 21:18:11 +0200

    Update impatient

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
-require("impatient").enable_profile()
-require("user")
+local impatient_ok, impatient = pcall(require, "impatient")
+if impatient_ok then
+	impatient.enable_profile()
+end
 
 if vim.g.vscode then
 	require("vscode")
