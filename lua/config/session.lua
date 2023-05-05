4d9c20c065574cb5fad12179e649763992aa7941
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-05-05 06:44:43 +0200

    refactor(session): don't automatically load a session

--- lua/config/session.lua
+++ lua/config/session.lua
@@ -1,13 +1,33 @@
-local session_string = "sessions"
-local session_file_name = "Session.vim"
 local logger = require("config.util.logger")
 local util = require("config.util")
+
+local session_string = "sessions"
+local session_file_name = "Session.vim"
 local api = vim.api
 local fn = vim.fn
 local cmd = vim.cmd
 
 local SessionGroup = api.nvim_create_augroup(session_string, { clear = true })
+local M = {}
+
 local is_file_readable = fn.filereadable(session_file_name) == 1 and 1
+
+local function create_session()
+	cmd.mksession({ mods = { emsg_silent = true } })
+	if is_file_readable then
+		logger.info({ msg = "New session started", title = session_string })
+	end
+end
+
+function M.load_session()
+	if is_file_readable then
+		cmd.source({ args = { session_file_name }, mods = { silent = false, emsg_silent = true } })
+
+		logger.info({ msg = "Loaded session!", title = session_string })
+	else
+		create_session()
+	end
+end
 
 api.nvim_create_autocmd("VimEnter", {
 	group = SessionGroup,
@@ -19,15 +39,10 @@ api.nvim_create_autocmd("VimEnter", {
 			return
 		end
 
-		if fn.filereadable(session_file_name) == 1 then
-			cmd.source({ args = { session_file_name }, mods = { silent = false, emsg_silent = true } })
-
-			vim.defer_fn(function() logger.info({ msg = "Loaded session!", title = session_string }) end, 500)
+		if is_file_readable then
+			logger.info({ msg = "Session file found", title = session_string })
 		else
-			cmd.mksession({ mods = { emsg_silent = true } })
-			if fn.filereadable(session_file_name) == 1 then
-				vim.defer_fn(function() logger.info({ msg = "Loaded session!", title = session_string }) end, 500)
-			end
+			create_session()
 		end
 	end,
 })
@@ -43,3 +58,5 @@ api.nvim_create_autocmd("VimLeavePre", {
 		cmd.mksession({ bang = true })
 	end,
 })
+
+return M
