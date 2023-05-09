41aa891af80c9a5f9936717a639f9102ce4632d2
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-05-09 10:23:13 +0200

    feat(dap): vscode plugin

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -7,6 +7,16 @@ M.dependencies = {
 	"theHamsta/nvim-dap-virtual-text",
 	"williamboman/mason.nvim",
 	{ "jay-babu/mason-nvim-dap.nvim", opts = { automatic_setup = true } },
+	{
+		"mxsdev/nvim-dap-vscode-js",
+		opts = function()
+			return {
+				debugger_path = require("mason-registry").get_package("js-debug-adapter"):get_install_path(),
+				debugger_cmd = { "js-debug-adapter" },
+				adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
+			}
+		end,
+	},
 }
 
 function M.config()
@@ -16,6 +26,11 @@ function M.config()
 	for name, sign in pairs(icons) do
 		vim.fn.sign_define("Dap" .. name, { text = sign })
 	end
+
+	require("dap.ext.vscode").load_launchjs(nil, {
+		["pwa-node"] = { "typescript" },
+		["node"] = { "typescript" },
+	})
 
 	dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
 	dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
