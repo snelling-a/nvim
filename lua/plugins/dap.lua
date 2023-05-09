b28bb400969096cd7a316d5a18877bf59deadc99
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-05-09 10:15:30 +0200

    feat(dap): dap-ui

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,3 +1,23 @@
+local icons = require("config.ui.icons").dap
+
 local M = { "mfussenegger/nvim-dap" }
+
+M.dependencies = {
+	"rcarriga/nvim-dap-ui",
+	"theHamsta/nvim-dap-virtual-text",
+}
+
+function M.config()
+	local dap = require("dap")
+	local dapui = require("dapui")
+
+	for name, sign in pairs(icons) do
+		vim.fn.sign_define("Dap" .. name, { text = sign })
+	end
+
+	dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+	dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+	dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+end
 
 return M
