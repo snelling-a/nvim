d0514cfc20caa7d0b318edee972e26938bba2cf9
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-05-10 22:45:36 +0200

    refactor(lsp): DRY utils

--- lua/config/lsp/util.lua
+++ lua/config/lsp/util.lua
@@ -1,5 +1,7 @@
 local util = require("config.util")
 
+local protocol = vim.lsp.protocol
+
 local M = {}
 
 ---wrapper for lspconfig.util.root_pattern
@@ -12,7 +14,7 @@ function M.get_root_pattern(config_files) return require("lspconfig").util.root_
 ---@return table
 function M.enable_broadcasting(opts)
 	--Enable (broadcasting) snippet capability for completion
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	local capabilities = protocol.make_client_capabilities()
 	capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 	opts.capabilities = util.tbl_extend_force(opts.capabilities or {}, capabilities)
@@ -23,7 +25,7 @@ end
 ---wrapper for lspconfig.util.root_pattern for graphql language servers
 ---@return function (startpath: any) -> string|unknown|nil
 function M.get_graphql_root_pattern()
-	return require("lspconfig").util.root_pattern({
+	return M.get_root_pattern({
 		".graphqlrc*",
 		".graphql.config.*",
 		"graphql.config.*",
