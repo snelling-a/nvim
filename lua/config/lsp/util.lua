107c5ae376f1c3aa59613df7f03613448551e0cd
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-05-10 22:38:41 +0200

    fix: types

--- lua/config/lsp/util.lua
+++ lua/config/lsp/util.lua
@@ -4,7 +4,7 @@ local M = {}
 
 ---wrapper for lspconfig.util.root_pattern
 ---@param config_files string[]
----@return _ function (startpath: any) -> string|unknown|nil
+---@return function (startpath: any) -> string|unknown|nil
 function M.get_root_pattern(config_files) return require("lspconfig").util.root_pattern(unpack(config_files)) end
 
 ---wrapper for tbl_extend.completion.completionItem.snippetSupport
@@ -21,7 +21,7 @@ function M.enable_broadcasting(opts)
 end
 
 ---wrapper for lspconfig.util.root_pattern for graphql language servers
----@return _ function (startpath: any) -> string|unknown|nil
+---@return function (startpath: any) -> string|unknown|nil
 function M.get_graphql_root_pattern()
 	return require("lspconfig").util.root_pattern({
 		".graphqlrc*",
