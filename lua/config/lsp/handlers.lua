eea1d3ee16c9d41d4febc9ffa2756630d7b2de9b
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-05-17 06:40:30 +0200

    feat(lsp): improve handlers

--- lua/config/lsp/handlers.lua
+++ lua/config/lsp/handlers.lua
@@ -1,19 +1,30 @@
 local fzf_lua = require("fzf-lua")
+local logger = require("config.util.logger")
 local handlers = vim.lsp.handlers
 local lsp = vim.lsp
+local util = vim.lsp.util
 
 local Handlers = {}
 
 function Handlers.on_attach()
+	handlers["callHierarchy/incomingCalls"] = fzf_lua.lsp_incoming_calls
+	handlers["callHierarchy/outgoingCalls"] = fzf_lua.lsp_outgoing_calls
+	handlers["textDocument/declaration"] = fzf_lua.lsp_declarations
+	handlers["textDocument/definition"] = function(_, result)
+		if not result or vim.tbl_isempty(result) then
+			logger.warn({ msg = "[LSP] Could not find definition", title = "LSP" })
+			return
+		end
+		if vim.tbl_islist(result) then
+			util.jump_to_location(result[1], "utf-8")
+		else
+			util.jump_to_location(result, "utf-8")
+		end
+	end
 	handlers["textDocument/documentSymbol"] = fzf_lua.lsp_document_symbols
-	-- handlers["textDocument/hover"] = lsp.with(handlers.hover, { border = "rounded" })
-	handlers["textDocument/publishDiagnostics"] =
-		lsp.with(handlers["textDocument/publishDiagnostics"], { signs = true, virtual_text = false })
+	handlers["textDocument/implementation"] = fzf_lua.lsp_implementations
+	handlers["textDocument/publishDiagnostics"] = lsp.with(lsp.diagnostic.on_publish_diagnostics, { signs = true })
 	handlers["textDocument/references"] = fzf_lua.lsp_references
-	-- handlers["textDocument/signatureHelp"] = lsp.with(handlers.signature_help, { border = "rounded" })
-	handlers["textdocument/definition"] = fzf_lua.lsp_definitions
-	handlers["textdocument/implementation"] = fzf_lua.lsp_implementations
-	handlers["textDocument/documentSymbol"] = fzf_lua.lsp_document_symbols
 	handlers["workspace/symbol"] = fzf_lua.lsp_workspace_symbols
 end
 
