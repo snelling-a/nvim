fd9fa35fa1eaea155afe7e2aae1238444b13f9f9
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-05-18 14:21:45 +0200

    style: add descriptions to user commands

--- lua/config/command.lua
+++ lua/config/command.lua
@@ -1,5 +1,5 @@
 local command = vim.api.nvim_create_user_command
 
-command("SortJSON", function() vim.cmd("%!jq . --sort-keys") end, { desc = "" })
+command("SortJSON", function() vim.cmd("%!jq . --sort-keys") end, { desc = "Sort json keys alphabetically" })
 
-command("SortYAML", function() vim.cmd("%!%!yq 'sort_keys(..)' %") end, { desc = "" })
+command("SortYAML", function() vim.cmd("%!yq 'sort_keys(..)' %") end, { desc = "Sort yaml keys alphabetically" })
