5fdb5cde91432caeb8f2aa60891c1246ee22312a
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-05-18 12:56:45 +0200

    feat: use notify as backup if noice doesn't load

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -42,7 +42,32 @@ local routes = {
 
 local M = { "folke/noice.nvim" }
 
-M.dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" }
+M.cond = require("config.util").is_vim()
+
+M.dependencies = {
+	"MunifTanjim/nui.nvim",
+	{
+		"rcarriga/nvim-notify",
+		keys = {
+			{
+				"<leader>un",
+				function() require("notify").dismiss({ silent = true, pending = true }) end,
+				desc = "Dismiss all Notifications",
+			},
+		},
+		opts = {
+			timeout = 3000,
+			max_height = function() return math.floor(vim.o.lines * 0.5) end,
+			max_width = function() return math.floor(vim.o.columns * 0.75) end,
+		},
+		init = function()
+			if not require("config.util").has("noice") then
+				vim.opt.termguicolors = true
+				vim.notify = require("notify")
+			end
+		end,
+	},
+}
 
 M.opts = {
 	cmdline = cmdline,
