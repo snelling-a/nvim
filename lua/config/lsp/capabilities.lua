cf7340ed4aed64a224115b59a3542a53f12d430a
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-05-21 16:32:10 +0200

    perf(lsp): extract capabilities from lsp/util

---
+++ lua/config/lsp/capabilities.lua
@@ -0,0 +1,55 @@
+local util = require("config.util")
+
+local protocol = vim.lsp.protocol
+
+---use default capabilities to create cmp capabilities
+---@param capabilities table
+---@return table
+local function cmp_wrapper(capabilities) return require("cmp_nvim_lsp").default_capabilities(capabilities) end
+
+local Capabilities = {}
+
+---wrapper for tbl_extend.completion.completionItem.snippetSupport
+---to enable (broadcasting) snippet capability for completion
+---@param opts table
+---@return table
+function Capabilities.enable_broadcasting(opts)
+	local capabilities = protocol.make_client_capabilities()
+	capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+	capabilities = cmp_wrapper(capabilities)
+
+	opts.capabilities = util.tbl_extend_force(opts.capabilities or {}, capabilities)
+
+	return opts
+end
+
+function Capabilities.get_capabilities()
+	local capabilities = protocol.make_client_capabilities()
+	capabilities = {
+		textDocument = {
+			foldingRange = { dynamicRegistration = false, lineFoldingOnly = true },
+			completion = {
+				completionItem = {
+					snippetSupport = true,
+					resolveSupport = { properties = { "documentation", "detail", "additionalTextEdits" } },
+				},
+			},
+			codeAction = {
+				resolveSupport = {
+					properties = vim.list_extend(capabilities.textDocument.codeAction.resolveSupport.properties, {
+						"documentation",
+						"detail",
+						"additionalTextEdits",
+					}),
+				},
+			},
+		},
+	}
+
+	capabilities = cmp_wrapper(capabilities)
+
+	return capabilities
+end
+
+return Capabilities
--- lua/config/lsp/util.lua
+++ lua/config/lsp/util.lua
@@ -1,7 +1,5 @@
 local util = require("config.util")
 
-local protocol = vim.lsp.protocol
-
 local M = {}
 
 ---wrapper for vim.api.nvim_buf_set_keymap
@@ -19,19 +17,6 @@ end
 ---@return function (startpath: any) -> string|unknown|nil
 function M.get_root_pattern(config_files) return require("lspconfig").util.root_pattern(unpack(config_files)) end
 
----wrapper for tbl_extend.completion.completionItem.snippetSupport
----@param opts table
----@return table
-function M.enable_broadcasting(opts)
-	--Enable (broadcasting) snippet capability for completion
-	local capabilities = protocol.make_client_capabilities()
-	capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-	opts.capabilities = util.tbl_extend_force(opts.capabilities or {}, capabilities)
-
-	return opts
-end
-
 ---wrapper for lspconfig.util.root_pattern for graphql language servers
 ---@return function (startpath: any) -> string|unknown|nil
 function M.get_graphql_root_pattern()
@@ -41,17 +26,6 @@ function M.get_graphql_root_pattern()
 		"graphql.config.*",
 		"relay.config.*",
 	})
-end
-
-function M.get_capabilities()
-	local capabilities = protocol.make_client_capabilities()
-	capabilities.textDocument.completion.completionItem.snippetSupport = true
-	capabilities.textDocument.completion.completionItem.resolveSupport = {
-		properties = { "documentation", "detail", "additionalTextEdits" },
-	}
-	capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-	return capabilities
 end
 
 return M
