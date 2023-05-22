08390c94a4ca39d325b98a02007af67209b8eaf8
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-05-22 22:12:08 +0200

    fix(flit): unmap commonly used keymaps

--- lua/plugins/flit.lua
+++ lua/plugins/flit.lua
@@ -6,19 +6,33 @@ M.dependencies = {
 	"tpope/vim-repeat",
 	{
 		"ggandor/leap.nvim",
-		keys = {
-			{ "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
-			{ "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
-			{ "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+		-- keys = {
+		-- 	{ "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+		-- 	{ "S", mode = { "n", "x", }, desc = "Leap backward to" },
+		-- 	{ "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+		-- },
+		opts = {
+			safe_labels = { "f", "n", "u", "t" },
+			labels = { "f", "n", "j", "k" },
+			special_keys = {
+				repeat_search = "<enter>",
+				next_phase_one_target = "<enter>",
+				next_target = { "<enter>", ";" },
+				prev_target = { "<tab>", "," },
+				next_group = "<space>",
+				prev_group = "<tab>",
+				multi_accept = "<enter>",
+				multi_revert = "<backspace>",
+			},
 		},
 		config = function(_, opts)
 			local leap = require("leap")
 			for k, v in pairs(opts) do
 				leap.opts[k] = v
 			end
-			leap.add_default_mappings(true)
-			vim.keymap.del({ "x", "o" }, "x")
-			vim.keymap.del({ "x", "o" }, "X")
+			-- leap.add_default_mappings(true)
+			-- vim.keymap.del({ "x", "o" }, "x")
+			-- vim.keymap.del({ "x", "o" }, "X")
 		end,
 	},
 }
