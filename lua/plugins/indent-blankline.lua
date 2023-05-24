f608a2231754379a2c2ccf6b8e6244e680e700e8
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-05-24 06:55:35 +0200

    perf: DRY indent-blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,20 +1,15 @@
-local icon = require("config.ui.icons").fillchars.foldsep
-local util = require("config.util")
-
 local M = { "lukas-reineke/indent-blankline.nvim" }
 
-M.cond = util.is_vim()
+M.cond = require("config.util").is_vim()
+
+local context_highlight_list = {}
+for i = 1, 6, 1 do
+	context_highlight_list[i] = "IndentBlanklineIndent" .. tostring(i)
+end
 
 M.opts = {
-	char = icon,
-	context_highlight_list = {
-		"IndentBlanklineIndent1",
-		"IndentBlanklineIndent2",
-		"IndentBlanklineIndent3",
-		"IndentBlanklineIndent4",
-		"IndentBlanklineIndent5",
-		"IndentBlanklineIndent6",
-	},
+	char = require("config.ui.icons").fillchars.foldsep,
+	context_highlight_list = context_highlight_list,
 	filetype_exclude = require("config.util.constants").no_format,
 	show_current_context = true,
 	show_foldtext = true,
