68adb03abe7ce701c3dc220dbd6585851c9fcc12
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-05-25 06:31:41 +0200

    feat(spell): SpellCheck command

--- lua/config/command.lua
+++ lua/config/command.lua
@@ -3,3 +3,9 @@ local command = vim.api.nvim_create_user_command
 command("SortJSON", function() vim.cmd("%!jq . --sort-keys") end, { desc = "Sort json keys alphabetically" })
 
 command("SortYAML", function() vim.cmd("%!yq 'sort_keys(..)' %") end, { desc = "Sort yaml keys alphabetically" })
+
+command(
+	"SpellCheck",
+	function() os.execute("cspell --unique --words-only --gitignore ** | sort > z_spell.txt") end,
+	{ desc = "Toggle spell check" }
+)
