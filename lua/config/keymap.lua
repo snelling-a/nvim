042087e9db83d161408799cce7625b5debff5d7d
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-07-03 11:51:30 +0200

    feat(keys): center page on certain actions
    - the mappings required tweaking both ttimeoutlen and timeoutlen

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -8,10 +8,41 @@ local map = vim.keymap.set
 vim.g.mapleader = ","
 
 local function escape() util.feedkeys("<Esc>", "i", true) end
+local function open_fold() util.feedkeys("zv") end
+local function act_center(lhs)
+	util.map({ "n" }, lhs, function()
+		util.feedkeys(lhs)
+		util.scroll_center()
+		open_fold()
+	end, { desc = "Center screen after " .. lhs })
+end
+local function center_act(lhs)
+	map({ "n" }, lhs, function()
+		util.scroll_center()
+		open_fold()
+		util.feedkeys(lhs)
+	end, { nowait = true, desc = "Center screen before " .. lhs })
+end
 
-util.map({ "c", "v", "x" }, "jk", escape, { desc = "Escape to normal mode" })
-util.map({ "n", "v" }, ":", ";", { desc = "Command is remapped to `;`" })
-map({ "n", "v" }, ";", ":", { desc = "Command is remapped to `;`" })
+act_center("G")
+act_center("N")
+act_center("n")
+act_center("}")
+act_center("{")
+act_center(")")
+act_center("(")
+center_act("<C-d>")
+center_act("<C-u>")
+center_act("A")
+center_act("a")
+center_act("C")
+center_act("c")
+center_act("I")
+center_act("i")
+center_act("O")
+center_act("o")
+center_act("S")
+center_act("s")
 
 map(
 	"n",
@@ -19,42 +50,43 @@ map(
 	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/I<Left><Left><Left>]],
 	{ desc = "[S]earch and replace word under the cursor" }
 )
-util.nmap("<C-u>", function()
-	util.feedkeys("<C-u>")
-	util.scroll_center()
-end, { desc = "Scroll [u]p and center" })
-util.nmap("<C-d>", function()
-	util.feedkeys("<C-d>")
-	util.scroll_center()
-end, { desc = "Scroll [d]own and center" })
-util.nmap("<C-z>", "<nop>", { desc = "I'm sure there is a use for this, but for now it's just annoying" })
-util.nmap("<S-TAB>", function() cmd.bprevious() end, { desc = "Go to the previous buffer" })
-util.nmap("<Tab>", function() cmd.bnext() end, { desc = "Go to the next buffer" })
+map({ "n", "v" }, ";", ":", { desc = "Command is remapped to `;`" })
+
+util.imap("jj", function() escape() end, { desc = "Go to normal mode" })
+util.imap("jk", function()
+	escape()
+	if vim.bo.buftype == "" then
+		cmd.update()
+	end
+end, { desc = "Write and go to normal mode" })
+
+util.map({ "c", "v", "x" }, "jk", escape, { desc = "Escape to normal mode" })
+util.map({ "n", "v" }, ":", ";", { desc = "Command is remapped to `;`" })
+
 util.mapL(",", "``", { desc = "Press `,,` to jump back to the last cursor position." })
 util.mapL("/", function()
 	cmd.nohlsearch()
 	logger.info("Highlighting cleared")
 end, { desc = "Clear search highlighting" })
+util.mapL("g", function()
+	local conf = logger.confirm({ msg = "Are you sure you want to quit?", type = "Warning" })
+	if conf == 1 then
+		cmd.quitall()
+	end
+end, { desc = "[Q]uit all windows" })
 util.mapL("x", function()
 	fn.setfperm(fn.expand("%:p"), "rwxr-xr-x")
 	logger.info({ msg = string.format("%s made executable", fn.expand("%")), title = "CHMOD!" })
 end, { desc = "Make file e[x]ecutable" })
+
+util.nmap("<C-z>", "<nop>", { desc = "I'm sure there is a use for this, but for now it's just annoying" })
+util.nmap("<S-TAB>", function() cmd.bprevious() end, { desc = "Go to the previous buffer" })
+util.nmap("<Tab>", function() cmd.bnext() end, { desc = "Go to the next buffer" })
 util.nmap("H", "^", { desc = "Move to the start of line" })
 util.nmap("J", "mzJ`z", { desc = "[J]oin next line to current line" })
 util.nmap("L", "$", { desc = "Move to the end of line" })
-util.nmap("N", function()
-	util.feedkeys("N")
-	util.scroll_center()
-	util.feedkeys("zv")
-end, { desc = "Go to previous search result and center" })
 util.nmap("U", function() cmd.redo() end, { desc = "Better redo" })
 util.nmap("Y", "y$", { desc = "[Y]ank to the end of the line" })
-util.nmap("n", function()
-	util.feedkeys("n")
-	util.scroll_center()
-	util.feedkeys("zv")
-end, { desc = "Go to [n]ext search result and center" })
-
 util.nmap("j", function()
 	if vim.v.count > 0 and vim.v.count >= 3 then
 		return ("m'" .. vim.v.count .. "j")
@@ -70,22 +102,7 @@ util.nmap("k", function()
 	end
 end, { noremap = true, expr = true, desc = "Move to previous visual line" })
 
-util.imap("jk", function()
-	escape()
-	if vim.bo.buftype == "" then
-		cmd.update()
-	end
-end, { desc = "Write and go to normal mode" })
-util.imap("jj", function() escape() end, { desc = "Go to normal mode" })
-
 util.vmap("<", "<gv", { desc = "Easy unindent" })
 util.vmap(">", ">gv", { desc = "Easy indent" })
 util.vmap("J", ":m '>+1<CR>gv=gv", { desc = "Move lines down" })
 util.vmap("K", ":m '<-2<CR>gv=gv", { desc = "Move lines up" })
-
-util.mapL("g", function()
-	local conf = logger.confirm({ msg = "Are you sure you want to quit?", type = "Warning" })
-	if conf == 1 then
-		cmd.quitall()
-	end
-end, { desc = "[Q]uit all windows" })
