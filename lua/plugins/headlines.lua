5b6423ab3824fda138d9e10f333dc8f24d3edf05
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-07-08 17:14:57 +0200

    perf(plugins): remove headlines
    - it was causing more problems than it's worth

--- lua/plugins/headlines.lua
+++ lua/plugins/headlines.lua
@@ -1,20 +1,21 @@
-local icons = require("config.ui.icons")
-local M = { "lukas-reineke/headlines.nvim" }
-
-M.dependencies = { "nvim-treesitter/nvim-treesitter" }
-
-M.ft = { "markdown" }
-
-M.opts = {
-	markdown = {
-		codeblock_highlight = "CodeBlock",
-		dash_highlight = "Dash",
-		dash_string = icons.misc.square,
-		fat_headlines = true,
-		fat_headline_lower_string = "",
-		quote_highlight = "Quote",
-		quote_string = icons.fillchars.vert,
-	},
-}
-
-return M
+-- local icons = require("config.ui.icons")
+-- local M = { "lukas-reineke/headlines.nvim" }
+--
+-- M.dependencies = { "nvim-treesitter/nvim-treesitter" }
+--
+-- M.ft = { "markdown" }
+--
+-- M.opts = {
+-- 	markdown = {
+-- 		codeblock_highlight = "CodeBlock",
+-- 		dash_highlight = "Dash",
+-- 		dash_string = icons.misc.square,
+-- 		fat_headlines = true,
+-- 		fat_headline_lower_string = "",
+-- 		quote_highlight = "Quote",
+-- 		quote_string = icons.fillchars.vert,
+-- 	},
+-- }
+--
+-- return M
+return {}
