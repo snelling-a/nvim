7671cbbeda11d00d8df30826a3429007c61db309
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-07-31 14:42:27 +0200

    style: descriptions for glance keymaps

--- lua/plugins/glance.lua
+++ lua/plugins/glance.lua
@@ -5,10 +5,10 @@ local Glance = { "dnlhc/glance.nvim" }
 Glance.event = "LspAttach"
 
 Glance.keys = {
-	{ "gd", function() vim.cmd.Glance("definitions") end, desc = "Show [d]efinition" },
-	{ "gr", function() vim.cmd.Glance("references") end, desc = "[G]et [r]eferences" },
-	{ "gy", function() vim.cmd.Glance("type_definitions") end, desc = "Show t[y]pe definition" },
-	{ "gi", function() vim.cmd.Glance("implementations") end, desc = "" },
+	{ "gd", function() vim.cmd.Glance("definitions") end, desc = "[G]lance at [d]efinition" },
+	{ "gr", function() vim.cmd.Glance("references") end, desc = "[G]lance at [r]eferences" },
+	{ "gy", function() vim.cmd.Glance("type_definitions") end, desc = "[G]lance at t[y]pe definition" },
+	{ "gi", function() vim.cmd.Glance("implementations") end, desc = "[G]lance at [i]mplementations" },
 }
 
 Glance.opts = {
