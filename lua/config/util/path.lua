b9000eeab731ce903b3b56056ea8708fe4ab3339
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-08-01 14:40:18 +0200

    feat: path logic

---
+++ lua/config/util/path.lua
@@ -0,0 +1,50 @@
+-- taken from https://github.com/williamboman/mason.nvim/blob/main/lua/mason-core/path.lua
+local function get_separator()
+	if jit then
+		local os = string.lower(jit.os)
+		if os == "linux" or os == "osx" or os == "bsd" then
+			return "/"
+		else
+			return "\\"
+		end
+	else
+		return string.sub(package.config, 1, 1)
+	end
+end
+
+local Path = {}
+
+---@param path_components string[]
+---@return string
+function Path.concat(path_components) return table.concat(path_components, get_separator()) end
+
+--- Path to all LSP server configurations
+---
+--- macos: `$XDG_CONFIG_HOME/nvim/lua/config/lsp/server`
+Path.lsp_servers = Path.concat({
+	vim.fn.stdpath("config"),
+	"lua",
+	"config",
+	"lsp",
+	"server",
+})
+
+--- Path to Mason.nvim packages
+---@param package_name string
+---@param args string|table<string>?
+---@return string
+function Path.get_mason_path(package_name, args)
+	local list = require("config.util").table_or_string(args)
+
+	local path_components = {
+		vim.fn.stdpath("data"),
+		"mason",
+		"packages",
+		package_name,
+		unpack(list),
+	}
+
+	return Path.concat(path_components)
+end
+
+return Path
