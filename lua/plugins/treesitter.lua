5671df6f6d55e9a5ba1a49a6e25517047094f352
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-08-07 16:48:15 +0200

    fix(treesitter): textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -37,48 +37,49 @@ local textobjects = {
 	select = {
 		enable = true,
 		lookahead = true,
-		lsp_interop = {
-			enable = true,
-			floating_preview_opts = { border = "rounded" },
-			peek_definition_code = { ["<leader>df"] = "@function.outer", ["<leader>dF"] = "@class.outer" },
-		},
 		keymaps = {
-			["aa"] = { query = "@parameter.outer", desc = "around a parameter" },
-			["ac"] = { query = "@class.outer", desc = "around a class" },
-			["af"] = { query = "@function.outer", desc = "around a function" },
-			["ai"] = { query = "@conditional.outer", desc = "around an if statement" },
-			["al"] = { query = "@loop.outer", desc = "around a loop" },
-			["ao"] = { query = "@block.outer", desc = "around a function block" },
-			["ia"] = { query = "@parameter.inner", desc = "inner part of a parameter" },
-			["ic"] = { query = "@class.inner", desc = "inner part of a class" },
-			["if"] = { query = "@function.inner", desc = "inner part of a function" },
-			["il"] = { query = "@loop.inner", desc = "inner part of a loop" },
-			["io"] = { query = "@block.inner", desc = "inner part of a function block" },
-			["it"] = { query = "@conditional.inner", desc = "inner part of an if statement" },
+			["aa"] = "@parameter.outer",
+			["ia"] = "@parameter.inner",
+			["af"] = "@function.outer",
+			["if"] = "@function.inner",
+			["ac"] = "@class.outer",
+			["ic"] = "@class.inner",
 		},
 	},
 	move = {
 		enable = true,
 		set_jumps = true,
-		goto_previous_start = {
-			["[f"] = { query = "@function.outer", desc = "Previous function" },
-			["[c"] = { query = "@class.outer", desc = "Previous class" },
-			["[p"] = { query = "@parameter.inner", desc = "Previous parameter" },
-		},
 		goto_next_start = {
-			["]f"] = { query = "@function.outer", desc = "Next function" },
-			["]c"] = { query = "@class.outer", desc = "Next class" },
-			["]p"] = { query = "@parameter.inner", desc = "Next parameter" },
+			["]f"] = "@function.outer",
+			["]]"] = "@class.outer",
+		},
+		goto_next_end = {
+			["]F"] = "@function.outer",
+			["]["] = "@class.outer",
+		},
+		goto_previous_start = {
+			["[m"] = "@function.outer",
+			["[["] = "@class.outer",
+		},
+		goto_previous_end = {
+			["[M"] = "@function.outer",
+			["[]"] = "@class.outer",
 		},
 	},
 	swap = {
 		enable = true,
-		swap_next = { ["<leader>a"] = "@parameter.inner" },
-		swap_previous = { ["<leader>A"] = "@parameter.inner" },
+		swap_next = {
+			["<leader>a"] = "@parameter.inner",
+		},
+		swap_previous = {
+			["<leader>A"] = "@parameter.inner",
+		},
 	},
 }
 
-local M = { "nvim-treesitter/nvim-treesitter" }
+local M = {
+	"nvim-treesitter/nvim-treesitter",
+}
 
 M.build = ":TSUpdate"
 
@@ -86,17 +87,33 @@ M.dependencies = {
 	"JoosepAlviste/nvim-ts-context-commentstring",
 	"nvim-treesitter/nvim-treesitter-textobjects",
 	"windwp/nvim-ts-autotag",
-	{ "nvim-treesitter/nvim-treesitter-context", opts = { mode = "topline" } },
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		opts = {
+			mode = "topline",
+		},
+	},
 }
 
 M.event = "BufEnter"
 
 M.opts = {
 	auto_install = true,
-	autotag = { enable = true },
-	context_commentstring = { enable = true, enable_autocmd = true },
+	autotag = {
+		enable = true,
+	},
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = true,
+	},
 	ensure_installed = ensure_installed,
-	highlight = { enable = true, additional_vim_regex_highlighting = { "markdown" } },
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = {
+			"markdown",
+			"markdown_inline",
+		},
+	},
 	incremental_selection = {
 		enable = true,
 		keymaps = {
@@ -106,13 +123,17 @@ M.opts = {
 			scope_incremental = false,
 		},
 	},
-	indent = { enable = true },
+	indent = {
+		enable = true,
+	},
 	textobjects = textobjects,
 }
 
 function M.config(_, opts)
 	require("nvim-treesitter.configs").setup(opts)
-	-- require("nvim-treesitter.install").compilers = { "gcc-13" } -- for neorg
+	require("nvim-treesitter.install").compilers = {
+		"gcc-13",
+	}
 end
 
 return M
