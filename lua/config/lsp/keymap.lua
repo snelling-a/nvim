184247ae0f5f52f42447e6a8d81411fbd64db896
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-08-13 16:34:26 +0200

    refactor(lsp): use bind function from lsp/util

--- lua/config/lsp/keymap.lua
+++ lua/config/lsp/keymap.lua
@@ -1,12 +1,13 @@
 local logger = require("config.util.logger")
+local bind = require("config.lsp.util").bind
 local util = require("config.util")
 
 local lsp = vim.lsp.buf
 local vim_diagnostic = vim.diagnostic
 
-local M = {}
+local LspKeymaps = {}
 
-function M.on_attach(bufnr)
+function LspKeymaps.on_attach(bufnr)
 	util.mapL("d", vim_diagnostic.open_float, { desc = "Open [d]iagnostic float" })
 	util.nmap("[d", function()
 		vim_diagnostic.goto_prev({ float = false })
@@ -17,34 +18,29 @@ function M.on_attach(bufnr)
 		util.scroll_center()
 	end, { desc = "Goto next [d]iagnostic issue" })
 
-	local function bind(target, source, desc)
-		local opts = { buffer = bufnr, desc = desc }
-
-		return util.nmap(target, source, opts)
-	end
-
-	-- bind("ca", lsp.code_action, "[C]ode [a]ction")
-	bind("<C-g>", lsp.signature_help, "Show signature help")
-	bind("<leader>gD", lsp.declaration, { desc = "Show [d]eclaration" })
-	bind("<leader>rn", lsp.rename, "[R]ename variable")
-	bind("<leader>wa", lsp.add_workspace_folder, "[A]dd [w]orkspace folder")
+	-- bind(bufnr,"ca", lsp.code_action, "[C]ode [a]ction")
+	bind(bufnr, "<C-g>", lsp.signature_help, "Show signature help")
+	bind(bufnr, "<leader>gD", lsp.declaration, "Show [d]eclaration")
+	bind(bufnr, "<leader>rn", lsp.rename, "[R]ename variable")
+	bind(bufnr, "<leader>wa", lsp.add_workspace_folder, "[A]dd [w]orkspace folder")
 	bind(
+		bufnr,
 		"<leader>wl",
 		function() logger.info(vim.inspect(lsp.list_workspace_folders())) end,
 		"[L]ist [w]orkspace folders"
 	)
-	bind("<leader>wr", lsp.remove_workspace_folder, "[R]emove [w]orkspace folder")
-	bind("K", lsp.hover, "Show hover")
-	bind("gD", function()
+	bind(bufnr, "<leader>wr", lsp.remove_workspace_folder, "[R]emove [w]orkspace folder")
+	bind(bufnr, "K", lsp.hover, "Show hover")
+	bind(bufnr, "gD", function()
 		lsp.definition()
 		util.scroll_center()
 	end, "Show [d]efinition")
-	bind("gR", function()
+	bind(bufnr, "gR", function()
 		lsp.references()
 		util.scroll_center()
 	end, "[G]et [r]eferences")
-	bind("gY", lsp.type_definition, "Show t[y]pe definition")
-	bind("gI", lsp.implementation, "Show [i]mplementation")
+	bind(bufnr, "gY", lsp.type_definition, "Show t[y]pe definition")
+	bind(bufnr, "gI", lsp.implementation, "Show [i]mplementation")
 end
 
-return M
+return LspKeymaps
