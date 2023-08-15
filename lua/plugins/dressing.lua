ba63de9c1205415dd4ec36bd45d01e1a6fdb4719
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-08-15 16:29:47 +0200

    style: formatting

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,30 +1,66 @@
-local M = { "stevearc/dressing.nvim" }
+local M = {
+	"stevearc/dressing.nvim",
+}
 
 M.lazy = true
 
 M.opts = {
 	get_config = function(opts)
 		if opts.kind == "codeaction" then
-			return { builtin = { relative = "cursor" } }
+			return {
+				builtin = {
+					relative = "cursor",
+				},
+			}
 		end
 	end,
 	input = {
 		default_prompt = require("config.util").get_prompt(""),
-		mappings = { i = { ["<C-p>"] = "HistoryPrev", ["<C-n>"] = "HistoryNext" } },
-		max_width = { 50, 0.3 },
-		min_width = { 30, 0.2 },
+		mappings = {
+			i = {
+				["<C-p>"] = "HistoryPrev",
+				["<C-n>"] = "HistoryNext",
+			},
+		},
+		max_width = {
+			50,
+			0.3,
+		},
+		min_width = {
+			30,
+			0.2,
+		},
 	},
-	nui = { relative = "cursor" },
+	nui = {
+		relative = "cursor",
+	},
 	select = {
-		backend = { "fzf_lua", "builtin" },
+		backend = {
+			"fzf_lua",
+			"builtin",
+		},
 		builtin = {
 			width = 30,
-			max_width = { 60, 0.3 },
-			min_width = { 30, 0.2 },
+			max_width = {
+				60,
+				0.3,
+			},
+			min_width = {
+				30,
+				0.2,
+			},
 			max_height = 0.3,
-			min_height = { 10, 0.2 },
+			min_height = {
+				10,
+				0.2,
+			},
 		},
-		fzf_lua = { winopts = { height = 0.4, width = 0.4 } },
+		fzf_lua = {
+			winopts = {
+				height = 0.4,
+				width = 0.4,
+			},
+		},
 		trim_prompt = false,
 	},
 }
@@ -33,15 +69,23 @@ function M.init()
 	local load = require("lazy").load
 	local ui = vim.ui
 
-	---@diagnostic disable-next-line: duplicate-set-field
+	--- @diagnostic disable-next-line: duplicate-set-field
 	ui.select = function(...)
-		load({ plugins = { "dressing.nvim" } })
+		load({
+			plugins = {
+				"dressing.nvim",
+			},
+		})
 		return ui.select(...)
 	end
 
-	---@diagnostic disable-next-line: duplicate-set-field
+	--- @diagnostic disable-next-line: duplicate-set-field
 	ui.input = function(...)
-		load({ plugins = { "dressing.nvim" } })
+		load({
+			plugins = {
+				"dressing.nvim",
+			},
+		})
 		return ui.input(...)
 	end
 end
