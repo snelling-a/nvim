b9e110352a5edebe5cbcc5dcfb0ab2fcdcff21ea
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-08-15 16:26:26 +0200

    style: use constant in lazy config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,4 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
@@ -12,24 +13,36 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	change_detection = { notify = false },
-	dev = { path = os.getenv("HOME") .. "/dev/github.com/snelling-a" },
-	install = { colorscheme = { "base16-default-dark", "habamax" } },
+	change_detection = {
+		notify = false,
+	},
+	dev = {
+		path = require("config.util.constants").dev_dir .. "/snelling-a",
+	},
+	install = {
+		colorscheme = {
+			"average-dark",
+			"base16-default-dark",
+			"habamax",
+		},
+	},
 	performance = {
 		rtp = {
 			disabled_plugins = {
 				"gzip",
-				"matchit",
+				-- "matchit",
 				"netrwPlugin",
 				"rplugin",
 				"spellfile",
 				"tarPlugin",
-				"tohtml",
+				-- "tohtml",
 				"tutor",
 				"zipPlugin",
 			},
 		},
 	},
-	spec = { import = "plugins" },
+	spec = {
+		import = "plugins",
+	},
 	ui = require("config.ui.icons").lazy,
 })
