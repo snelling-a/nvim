9cdbe7c72893e52e9087d61b5a50c449e1a1d25b
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-08-16 18:12:21 +0200

    fix: lf

--- lua/plugins/lf.lua
+++ lua/plugins/lf.lua
@@ -1,19 +1,45 @@
-local LF = { "lmburns/lf.nvim" }
+local LF = {
+	"lmburns/lf.nvim",
+}
 
-LF.dependencies = { "nvim-lua/plenary.nvim", "akinsho/toggleterm.nvim" }
+LF.dependencies = {
+	"nvim-lua/plenary.nvim",
+	"akinsho/toggleterm.nvim",
+}
 
 LF.lazy = false
 
-function LF.config()
+LF.keys = {
+	{
+		"<M-o>",
+		vim.cmd.Lf,
+		desc = "Open lf",
+	},
+	{
+		"<M-u",
+		desc = "Resize lf window",
+	},
+}
+
+LF.opts = {
+	default_actions = {
+		["<C-s>"] = "split",
+	},
+}
+
+function LF.config(_, opts)
 	vim.g.lf_netrw = 1
 
-	require("lf").setup({
-		default_actions = { ["<C-s>"] = "split" },
-		border = "rounded",
-		direction = "horizontal",
-	})
+	require("lf").setup(opts)
 
-	vim.keymap.set("n", "<leader>l", "<cmd>lua require('lf').start()<CR>", { noremap = true })
+	vim.api.nvim_create_autocmd("User", {
+		pattern = "LfTermEnter",
+		callback = function(a)
+			vim.api.nvim_buf_set_keymap(a.buf, "t", "q", "q", {
+				nowait = true,
+			})
+		end,
+	})
 end
 
 return LF
