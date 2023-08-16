4c9f5ce66dac01a6c9b42aea21fb86277ab41ff7
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-08-16 10:05:36 +0200

    fix: import cost

--- lua/plugins/import-cost.lua
+++ lua/plugins/import-cost.lua
@@ -1,9 +1,15 @@
-local ImportCost = { "barrett-ruth/import-cost.nvim" }
+local ImportCost = {
+	"barrett-ruth/import-cost.nvim",
+}
 
 ImportCost.build = "sh install.sh yarn"
 
-ImportCost.opts = { highlight = "TSComment" }
+ImportCost.opts = {
+	highlight = "TSComment",
+}
 
-ImportCost.ft = require("config.util.constants").javascript_typescript
+ImportCost.ft = vim.fn.deepcopy(require("config.util.constants").javascript_typescript)
+
+ImportCost.config = true
 
 return ImportCost
