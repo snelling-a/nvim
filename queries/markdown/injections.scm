87411216d353edd442a86389efdcadf7da9df144
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-08-17 09:00:28 +0200

    fix(treesitter): remove exclude children directive

--- queries/markdown/injections.scm
+++ queries/markdown/injections.scm
@@ -5,7 +5,6 @@
     (language) @_lang)
   (code_fence_content) @content
   (#set-lang-from-info-string! @_lang)
-  (#exclude_children! @content)
   )
 
 ((html_block) @html @combined)
@@ -17,6 +16,6 @@
   (inline)
   (pipe_table_cell)
   ]
- @markdown_inline 
- (#exclude_children! @markdown_inline))
+ @markdown_inline
+ )
 
