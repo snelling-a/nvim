5a356a12a6a8c4a2ee7a359ef0887b26b1cd6266
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-08-28 21:50:41 +0200

    fix(obsidian): note_id_function

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -2,30 +2,69 @@ local obsidian = require("config.util.constants").obsidian
 local cmd = vim.cmd
 
 local function note_id_func(input)
-	local title = "Untitled"
 	if input ~= nil then
-		title = input:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+		input = input:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
 	end
 
-	return os.date("%Y%m%d%H%M") .. "-" .. title
+	local title = (input and "-" .. input) or ""
+	return os.date("%Y%m%d%H%M") .. title
 end
 
-local Obsidian = { "epwalsh/obsidian.nvim" }
 
-Obsidian.dependencies =
-	{ "hrsh7th/nvim-cmp", "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" }
 
-Obsidian.event = { "BufReadPre " .. obsidian.vault_directory .. "/*" }
+local M = {
+	"epwalsh/obsidian.nvim",
+}
 
-Obsidian.ft = "markdown"
+M.dependencies = {
+	"hrsh7th/nvim-cmp",
+	"ibhagwan/fzf-lua",
+	"nvim-lua/plenary.nvim",
+	"nvim-treesitter/nvim-treesitter",
+}
 
-Obsidian.keys = {
-	{ "<leader>dn", cmd.ObsidianToday, desc = "Open today's [d]aily [n]ote" },
-	{ "<leader>ob", cmd.ObsidianBacklinks, desc = "[O]pen [b]acklinks" },
-	{ "<leader>ol", cmd.ObsidianLinkNew, desc = "[O]pen [l]ink in new buffer" },
-	{ "<leader>on", cmd.ObsidianNew, desc = "[O]pen [n]ew note" },
-	{ "<leader>ot", cmd.ObsidianToday, desc = "[O]pen [t]oday's daily note" },
-	{ "<leader>oy", cmd.ObsidianYesterday, desc = "[O]pen [y]esterday's daily note" },
+M.event = {
+	"BufReadPre " .. obsidian.vault_directory .. "/*",
+}
+
+M.ft = "markdown"
+
+M.keys = {
+	{
+		"<leader>dn",
+		cmd.ObsidianToday,
+		desc = "Open today's [d]aily [n]ote",
+	},
+	{
+		"<leader>ob",
+		cmd.ObsidianBacklinks,
+		desc = "[O]pen [b]acklinks",
+	},
+	{
+		"<leader>ol",
+		cmd.ObsidianLinkNew,
+		desc = "[O]pen [l]ink in new buffer",
+	},
+	{
+		"<leader>on",
+		cmd.ObsidianNew,
+		desc = "[O]pen [n]ew note",
+	},
+	{
+		"<leader>os",
+		cmd.ObsidianSearch,
+		desc = "[O]bsidian [s]earch",
+	},
+	{
+		"<leader>ot",
+		cmd.ObsidianToday,
+		desc = "[O]pen [t]oday's daily note",
+	},
+	{
+		"<leader>oy",
+		cmd.ObsidianYesterday,
+		desc = "[O]pen [y]esterday's daily note",
+	},
 	{
 		"gf",
 		function()
@@ -39,15 +78,24 @@ Obsidian.keys = {
 	},
 }
 
-Obsidian.lazy = not obsidian.is_vault_directory
+M.lazy = not obsidian.is_vault_directory
 
-Obsidian.opts = {
-	completion = { nvim_cmp = true },
-	daily_notes = { folder = "daily_notes" },
+M.opts = {
+	completion = {
+		nvim_cmp = true,
+	},
+	daily_notes = {
+		folder = "daily_notes",
+	},
 	dir = obsidian.vault_directory,
+	finder = "fzf-lua",
 	note_id_func = note_id_func,
 	open_app_foreground = true,
+	templates = {
+		subdir = "templates",
+	},
 	use_advanced_uri = true,
+	note_frontmatter_func = note_frontmatter_func,
 }
 
-return Obsidian
+return M
