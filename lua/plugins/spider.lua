1a2c23ce397b45faf8a831cd7fbf8de10a561493
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-08-28 21:51:28 +0200

    feat: nvim-spider

---
+++ lua/plugins/spider.lua
@@ -0,0 +1,34 @@
+local M = {
+	"chrisgrieser/nvim-spider",
+}
+
+function M.keys()
+	local mode = {
+		"n",
+		"o",
+		"x",
+	}
+	local motions = {
+		"w",
+		"e",
+		"b",
+		"ge",
+	}
+
+	local keys = {}
+
+	for _, v in ipairs(motions) do
+		table.insert(keys, {
+			v,
+			function() require("spider").motion(v) end,
+			desc = string.format("Spider-%s", v),
+			mode = mode,
+		})
+	end
+
+	return keys
+end
+
+M.lazy = true
+
+return M
