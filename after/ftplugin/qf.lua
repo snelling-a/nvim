af1b2090d62148ddc0a2b8046ea8007d3c210c27
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-09-01 22:22:23 +0200

    feat(qf): easy close/cfilter

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -2,3 +2,5 @@ vim.opt_local.listchars = {
 	space = " ",
 }
 
+require("config.util").nmap("<C-c>", ":cclose<CR>", { buffer = true, desc = "Close quickfix" })
+
--- lua/config/init.lua
+++ lua/config/init.lua
@@ -9,3 +9,9 @@ require("config.opt")
 require("config.session")
 require("config.ui")
 -- require("config.util.netrw")
+
+vim.cmd([[packadd cfilter]])
+if vim.fn.executable("fzf") == 1 then
+	-- [[set rtp+=/usr/local/opt/fzf]]
+	vim.opt.rtp:append("/usr/local/opt/fzf")
+end
