b16bbdfa93d222bfd866ceeedb135431f4ec4065
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-09-07 16:57:47 +0200

    style: formatting

--- lua/config/lsp/keymap.lua
+++ lua/config/lsp/keymap.lua
@@ -29,30 +29,22 @@ function M.on_attach(bufnr)
         desc = "Goto next [d]iagnostic issue",
     })
 
-    bind(bufnr, "<leader>ca", lsp.code_action, "[C]ode [a]ction")
-    bind(bufnr, "<C-g>", lsp.signature_help, "Show signature help")
-    bind(bufnr, "<leader>gD", function()
-        local loc = vim.lsp.util.make_position_params()
-        vim.lsp.util.jump_to_location(loc, "") --
-    end, "Show [d]eclaration")
-    bind(bufnr, "<leader>rn", lsp.rename, "[R]ename variable")
-    bind(bufnr, "<leader>wa", lsp.add_workspace_folder, "[A]dd [w]orkspace folder")
-    bind(bufnr, "<leader>wl", function() Logger.info(lsp.list_workspace_folders()) end, "[L]ist [w]orkspace folders")
-    bind(bufnr, "<leader>wr", lsp.remove_workspace_folder, "[R]emove [w]orkspace folder")
-    bind(bufnr, "K", lsp.hover, "Show hover")
-    bind(bufnr, "gd", function()
-        lsp.definition()
-        Util.scroll_center()
-    end, "Show [d]efinition")
-    bind(bufnr, "gr", function()
-        Util.scroll_center()
-    end, "[G]et [r]eferences")
-    bind(bufnr, "gY", lsp.type_definition, "Show t[y]pe definition")
-    bind(bufnr, "gI", lsp.implementation, "Show [i]mplementation")
+	bind(bufnr, "<leader>ca", lsp.code_action, "[C]ode [a]ction")
+	bind(bufnr, "<C-g>", lsp.signature_help, "Show signature help")
+	bind(bufnr, "<leader>rn", lsp.rename, "[R]ename variable")
+	bind(bufnr, "K", lsp.hover, "Show hover")
+	bind(bufnr, "gd", function()
+		lsp.definition()
+		Util.scroll_center()
+	end, "Show [d]efinition")
 	bind(bufnr, "gr", function()
 		lsp.references({
 			includeDeclaration = false,
 		})
+		Util.scroll_center()
+	end, "[G]et [r]eferences")
+	bind(bufnr, "gY", lsp.type_definition, "Show t[y]pe definition")
+	bind(bufnr, "gI", lsp.implementation, "Show [i]mplementation")
 end
 
 return M
