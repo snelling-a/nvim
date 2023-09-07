59eedc1ceeee1fc758be73d73daa7e75c19bbb1d
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-09-07 09:23:35 +0200

    style(lsp): consistent config_files variables

--- lua/config/lsp/server/graphql.lua
+++ lua/config/lsp/server/graphql.lua
@@ -1,13 +1,15 @@
+local config_files = {
+	".graphqlrc*",
+	".graphql.config.*",
+	"graphql.config.*",
+}
+
 local M = {}
 
 M.mason_name = "graphql-language-service-cli"
 
 function M.setup(opts)
-	opts.root_dir = require("config.lsp.util").get_root_pattern({
-		".graphqlrc*",
-		".graphql.config.*",
-		"graphql.config.*",
-	})
+	opts.root_dir = require("config.lsp.util").get_root_pattern(config_files)
 
 	require("lspconfig").graphql.setup(opts)
 end
