85f41e34e5091adbf6ae3b50bc1e849f1d97121c
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-09-12 12:28:41 +0200

    style: ignore warning

--- lua/config/lsp/server/tsserver.lua
+++ lua/config/lsp/server/tsserver.lua
@@ -3,6 +3,7 @@ local M = {}
 M.mason_name = "typescript-language-server"
 
 -- luacheck: no unused args
+---@diagnostic disable-next-line: unused-local
 function M.setup(opts) return nil end
 
 return M
