629204bdf0d2e1def092a3fc37df6f5c52803344
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-09-12 09:08:51 +0200

    fix(session): export get_separator method

--- lua/config/util/path.lua
+++ lua/config/util/path.lua
@@ -1,5 +1,7 @@
+local M = {}
+
 -- taken from https://github.com/williamboman/mason.nvim/blob/main/lua/mason-core/path.lua
-local function get_separator()
+function M.get_separator()
 	if jit then
 		local os = string.lower(jit.os)
 		if os == "linux" or os == "osx" or os == "bsd" then
@@ -12,16 +14,14 @@ local function get_separator()
 	end
 end
 
-local M = {}
-
 --- @param path_components string[]
 --- @return string
-function M.concat(path_components) return table.concat(path_components, get_separator()) end
+function M.concat(path_components) return table.concat(path_components, M.get_separator()) end
 
 --- Path to all LSP server configurations
 --- macos: `~/.config/nvim/lua/config/lsp/server`
 M.lsp_servers = M.concat({
----@diagnostic disable-next-line: assign-type-mismatch
+	---@diagnostic disable-next-line: assign-type-mismatch
 	vim.fn.stdpath("config"),
 	"lua",
 	"config",
@@ -32,7 +32,7 @@ M.lsp_servers = M.concat({
 --- Path to all EFM linters/formatters
 --- macos: `~/.config/nvim/lua/config/lsp/efm`
 M.linters_formatters = M.concat({
----@diagnostic disable-next-line: assign-type-mismatch
+	---@diagnostic disable-next-line: assign-type-mismatch
 	vim.fn.stdpath("config"),
 	"lua",
 	"config",
