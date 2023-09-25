30468db691be3702995538f72899d1b4d1d7ad59
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-09-26 00:04:18 +0200

    refactor(lsp): toggle inlay hints

--- lua/config/lsp/inlay_hint.lua
+++ lua/config/lsp/inlay_hint.lua
@@ -1,3 +1,27 @@
+_G.INLAY_HINTS = true
+
+--- @param bufnr integer
+local function setup_user_commands(bufnr)
+	local Logger = require("config.util.logger"):new("Inlay hints")
+
+	vim.api.nvim_create_user_command("InlayHintToggle", function()
+		_G.INLAY_HINTS = not _G.INLAY_HINTS
+
+		vim.lsp.inlay_hint(bufnr, _G.INLAY_HINTS)
+
+		local state = require("config.lsp.util").get_state(_G.INLAY_HINTS)
+		local str = ("Inlay hints have been %s."):format(state)
+
+		if _G.INLAY_HINTS then
+			Logger:info(str)
+		else
+			Logger:warn(str)
+		end
+	end, {
+		desc = "Toggle LSP inlay hints",
+	})
+end
+
 local M = {}
 
 --- @param client lsp.Client
@@ -9,11 +33,9 @@ function M.on_attach(client, bufnr)
 		return
 	end
 
-	vim.lsp.inlay_hint(bufnr, true)
+	vim.lsp.inlay_hint(bufnr, _G.INLAY_HINTS)
 
-	vim.api.nvim_create_user_command("InlayHintToggle", function() vim.lsp.inlay_hint(0, nil) end, {
-		desc = "Toggle LSP inlay hints",
-	})
+	setup_user_commands(bufnr)
 end
 
 return M
