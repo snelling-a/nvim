8d6da33f6833282bd6f5882d2373bab57b304b62
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-09-25 10:15:41 +0200

    fix: abbrev

--- lua/config/abbrev.lua
+++ lua/config/abbrev.lua
@@ -1,9 +1,10 @@
-local abbrev = vim.cmd.abbreviate
+local iabbrev = vim.cmd.iabbrev
 
-abbrev(":note:", [[<c-r>=printf(&commentstring, 'NOTE '.$USER.' ('.strftime("%d/%m/%y").'):')<CR>]])
-abbrev(":todo:", [[<c-r>=printf(&commentstring, 'TODO('.$USER.'):')<CR>]])
-abbrev("function", "function")
+iabbrev(":note:", [[<c-r>=printf(&commentstring, 'NOTE '.$USER.' ('.strftime("%d/%m/%y").'):')<CR>]])
+iabbrev(":todo:", [[<c-r>=printf(&commentstring, 'TODO('.$USER.'):')<CR>]])
+iabbrev("clo", "console.log()<left>")
 vim.cmd.abbrev("<expr>", "%%", "expand('%:p:h')")
 
 vim.cmd.cabbrev("Q", "q")
-vim.cmd.cabbrev("lpr", "lua print(")
+vim.cmd.cabbrev("lpr", "lua print()<left>")
+vim.cmd.cabbrev("lpi", "lua print(vim.inspect())<left><left>")
