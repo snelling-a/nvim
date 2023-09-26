499324674b3c1e7685e993c8a313773abe91da64
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-09-26 18:15:51 +0200

    chore: remove deprecated shortmess flags

--- lua/config/ui/shortmess.lua
+++ lua/config/ui/shortmess.lua
@@ -1,14 +1,11 @@
 local opt = vim.opt
 
 local shortmess_append = {
-	--  f	use "(3 of 5)" instead of "(file 3 of 5)"
-	--  i	use "[noeol]" instead of "[Incomplete last line]"
 	--  l	use "999L, 888B" instead of "999 lines, 888 bytes"
 	--  m	use "[+]" instead of "[Modified]"
-	--  n	use "[New]" instead of "[New File]"
 	--  r	use "[RO]" instead of "[readonly]"
 	--  w	use "[w]" instead of "written" for file write message and "[a]" instead of "appended" for ':w >> file' command
-	--  x	use "[dos]" instead of "[dos format]", "[unix]" instead of "[unix format]" and "[mac]" instead of "[mac format]"
+	"A", -- don't give the "ATTENTION" message when an existing swap file is found
 	"a", --  all of the above abbreviations
 	"c", --  don't give ins-completion-menu messages
 	"F", -- don't give the file info when editing a file, like `:silent` was used for the command
@@ -23,6 +20,7 @@ local shortmess_append = {
 local shortmess_remove = {
 	"O", -- message for reading a file overwrites any previous message; also for quickfix message (e.g., ":cn")
 	"T", -- Truncate other messages in the middle if they are too long to fit on the command line
+	"C", --  don't give |ins-completion-menu| messages
 	"S", --  do not show search count message when searching, e.g. "[1/5]"
 }
 
