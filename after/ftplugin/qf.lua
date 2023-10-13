b4f38466c86d78e2bf79b1dafe80a406312f4cd9
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-10-13 07:07:37 +0200

    style: vim string -> lua function

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -2,5 +2,7 @@ vim.opt_local.listchars = {
 	space = " ",
 }
 
-require("config.util").nmap("<C-c>", ":cclose<CR>", { buffer = true, desc = "Close quickfix" })
-
+require("config.util").nmap("<C-c>", function() vim.cmd.cclose() end, {
+	buffer = true,
+	desc = "Close quickfix",
+})
