e7dec0bdffc30797e8195897c2caee8a64cc4249
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-10-24 20:38:08 +0300

    feat(cmdline): wilder branches

--- lua/plugins/wilder.lua
+++ lua/plugins/wilder.lua
@@ -17,6 +17,29 @@ function M.config()
 	})
 
 	wilder.set_option("use_python_remote_plugin", 0)
+	wilder.set_option("pipeline", {
+		wilder.branch(
+			wilder.substitute_pipeline({
+				pipeline = wilder.vim_search_pipeline({
+					hide_in_replace = true,
+				}),
+			}),
+			wilder.cmdline_pipeline({
+				fuzzy = 2,
+				hide_in_substitute = false,
+				language = "vim",
+			}),
+			wilder.vim_search_pipeline(),
+			{
+				wilder.history(0),
+				wilder.result({
+					draw = {
+						function() return "🤷" end,
+					},
+				}),
+			}
+		),
+	})
 end
 
 return M
