89284925a006ed7748d082899a08e58f26057626
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-10-24 20:40:36 +0300

    feat(cmdline): popup/highlighting

--- lua/plugins/wilder.lua
+++ lua/plugins/wilder.lua
@@ -17,6 +17,7 @@ function M.config()
 	})
 
 	wilder.set_option("use_python_remote_plugin", 0)
+
 	wilder.set_option("pipeline", {
 		wilder.branch(
 			wilder.substitute_pipeline({
@@ -40,6 +41,58 @@ function M.config()
 			}
 		),
 	})
+
+	local gradient_colors = {
+		vim.g.base16_08,
+		vim.g.base16_09,
+		vim.g.base16_0A,
+		vim.g.base16_0B,
+		vim.g.base16_0C,
+		vim.g.base16_0D,
+		vim.g.base16_0E,
+		vim.g.base16_0F,
+	}
+
+	local gradient = {}
+	for i, fg in ipairs(gradient_colors) do
+		gradient[i] = wilder.make_hl("WilderGradient" .. i, "Pmenu", {
+			{
+				a = 1,
+			},
+			{
+				a = 1,
+			},
+			{
+				foreground = fg,
+			},
+		})
+	end
+
+	wilder.set_option(
+		"renderer",
+		wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+			highlights = {
+				default = "Pmenu",
+				border = "PmenuBorder",
+				gradient = gradient,
+			},
+			border = "rounded",
+			highlighter = wilder.highlighter_with_gradient({
+				wilder.vim_basic_highlighter(),
+			}),
+			left = {
+				" ",
+				wilder.popupmenu_devicons(),
+			},
+			right = {
+				" ",
+				wilder.popupmenu_scrollbar(),
+			},
+			empty_message = wilder.popupmenu_empty_message_with_spinner({
+				done = "🤷",
+			}),
+		}))
+	)
 end
 
 return M
