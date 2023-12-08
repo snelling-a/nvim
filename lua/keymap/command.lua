72303d17a5ba5c0737ce5d558ad42abc3f2cb82c
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-12-08 11:16:38 +0100

    chore: update plugins

--- lua/keymap/command.lua
+++ lua/keymap/command.lua
@@ -6,3 +6,16 @@ map("make", function()
 	feedkeys("<Enter>")
 	vim.cmd.copen()
 end, { desc = "Make and open the quickfix list" })
+
+local function wild(lhs, rhs)
+	map(lhs, function()
+		if vim.fn.wildmenumode() == 1 then
+			return lhs
+		else
+			return rhs
+		end
+	end, { desc = ("Navigate %s in wildmenu"):format(rhs), expr = true, silent = false })
+end
+
+wild("<C-n>", "<down>")
+wild("<C-p>", "<up>")
