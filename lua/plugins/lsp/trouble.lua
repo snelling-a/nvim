a192f7d2464ea56202f06e0a6541f27fcb3af30f
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-12-15 15:02:11 +0100

    style: remove unnecessary keymapping

--- lua/plugins/lsp/trouble.lua
+++ lua/plugins/lsp/trouble.lua
@@ -10,8 +10,6 @@ M.opts = { auto_close = true, use_diagnostic_signs = true }
 ---@diagnostic disable-next-line: assign-type-mismatch
 function M.keys()
 	local trouble_toggle = require("trouble").toggle
-	local opts = { skip_groups = true, jump = true }
-	local try = require("util").try
 
 	return {
 		{
@@ -34,28 +32,6 @@ function M.keys()
 				trouble_toggle("document_diagnostics")
 			end,
 			desc = "Toggle trouble for [d]ocument",
-		},
-		{
-			"[q",
-			function()
-				if require("trouble").is_open() then
-					require("trouble").previous(opts)
-				else
-					try(vim.cmd.cprev, { msg = "No errors" })
-				end
-			end,
-			desc = "Previous trouble/quickfix item",
-		},
-		{
-			"]q",
-			function()
-				if require("trouble").is_open() then
-					require("trouble").next(opts)
-				else
-					try(vim.cmd.next, { msg = "No errors" })
-				end
-			end,
-			desc = "Next trouble/quickfix item",
 		},
 	}
 end
