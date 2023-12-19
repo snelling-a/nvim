c1d6c541ff47f596b4af411e39755eecb854e74c
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-12-19 15:13:14 +0100

    perf: luasnip/copilot

--- lua/plugins/completion/luasnip.lua
+++ lua/plugins/completion/luasnip.lua
@@ -15,18 +15,30 @@ M.dependencies = {
 
 M.event = { "InsertEnter" }
 
-function M.opts()
-	vim.tbl_map(function(type)
-		require(("luasnip.loaders.from_%s"):format(type)).lazy_load()
-	end, {
-		"vscode",
-		"lua",
-	})
+M.opts = {
+	delete_check_events = "InsertLeave",
+	history = true,
+}
 
-	return {
-		delete_check_events = "InsertLeave",
-		history = true,
-	}
+function M.config(_, opts)
+	local luasnip = require("luasnip")
+	luasnip.setup(opts)
+
+	for _, type in pairs({ "vscode", "lua" }) do
+		require(("luasnip.loaders.from_%s"):format(type)).lazy_load()
+	end
+
+	vim.api.nvim_create_autocmd("User", {
+		callback = function()
+			local trigger = not luasnip.expand_or_locally_jumpable()
+
+			vim.b.copilot_suggestion_auto_trigger = trigger
+			vim.b.copilot_suggestion_hidden = not trigger
+		end,
+		desc = "Disable Copilot suggestions when Luasnip is active",
+		group = require("autocmd").augroup("CopilotSnippetSuggestions"),
+		pattern = { "LuasnipInsertNodeEnter", "LuasnipInsertNodeLeave" },
+	})
 end
 
 return M
