dcac5b9b603da65294b142abbe7c6fb5af6872b5
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-12-19 15:07:28 +0100

    perf: cmp path/cmdline
    - use async-path
    - don't use command line

--- lua/plugins/completion/cmp.lua
+++ lua/plugins/completion/cmp.lua
@@ -2,15 +2,11 @@
 local M = { "hrsh7th/nvim-cmp" }
 
 M.dependencies = {
+	"FelipeLema/cmp-async-path",
+	"L3MON4D3/LuaSnip",
 	"hrsh7th/cmp-buffer",
 	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/cmp-path",
-	"L3MON4D3/LuaSnip",
 	"saadparwaiz1/cmp_luasnip",
-	{
-		"hrsh7th/cmp-cmdline",
-		event = { "CmdlineEnter" },
-	},
 }
 
 M.event = { "InsertEnter" }
@@ -24,51 +20,19 @@ function M.config(_, opts)
 		source.group_index = source.group_index or 1
 	end
 
-	vim.opt.completeopt = { "menu", "menuone", "noselect" }
-	local cmp = require("cmp")
+	require("cmp").setup(opts)
 
-	cmp.setup(opts)
-
-	---@diagnostic disable-next-line: missing-fields
-	cmp.setup.cmdline("/", {
-		mapping = cmp.mapping.preset.cmdline(),
-		sources = {
-			{ name = "buffer" },
-		},
-	})
-
-	---@diagnostic disable-next-line: missing-fields
-	cmp.setup.cmdline(":", {
-		mapping = cmp.mapping.preset.cmdline({
-			["<C-n>"] = cmp.mapping.select_next_item(),
-			["<C-p>"] = cmp.mapping.select_prev_item(),
-			["<CR>"] = function(fallback)
-				if cmp.visible() and cmp.get_selected_entry() then
-					cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-					cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
-				else
-					fallback()
-				end
-			end,
-		}),
-		sources = cmp.config.sources({
-			{ name = "path" },
-			{ name = "cmdline" },
-		}),
-	})
+	if opts.experimental.ghost_text then
+		vim.api.nvim_set_hl(0, "CmpGhostText", { link = "TSComment", default = true })
+	end
 end
 
 ---@return cmp.Config
 function M.opts()
-	vim.api.nvim_set_hl(0, "CmpGhostText", { link = "TSComment", default = true })
 	local cmp = require("cmp")
-	local defaults = require("cmp.config.default")()
-	local luasnip = require("luasnip")
 
 	---@type cmp.ConfigSchema
-	---@diagnostic disable-next-line: missing-fields
 	return {
-		---@diagnostic disable-next-line: missing-fields
 		snippet = {
 			expand = function(args)
 				vim.snippet.expand(args.body)
@@ -80,15 +44,14 @@ function M.opts()
 				cmp.abort()
 				fallback()
 			end,
-			["<C-e>"] = cmp.mapping.abort(),
 			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-			["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 			["<C-Space>"] = cmp.mapping.complete(),
 			["<CR>"] = cmp.mapping.confirm({ select = true }),
 			["<S-CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
 			["<Tab>"] = cmp.mapping(function(fallback)
-				if luasnip.expand_or_locally_jumpable() then
+				local luasnip = require("luasnip")
+
+				if luasnip.expand_or_jump() then
 					luasnip.expand_or_jump()
 				else
 					fallback()
@@ -97,8 +60,8 @@ function M.opts()
 		}),
 		sources = cmp.config.sources({
 			{ name = "nvim_lsp", group_index = 1 },
-			{ name = "luasnip" },
-			{ name = "path", group_index = 1 },
+			{ name = "luasnip", group_index = 1 },
+			{ name = "async_path", group_index = 1 },
 			{ name = "buffer", group_index = 2 },
 		}),
 		---@diagnostic disable-next-line: missing-fields
@@ -114,15 +77,11 @@ function M.opts()
 			end,
 		},
 		window = {
-			---@diagnostic disable-next-line: missing-fields
 			completion = { border = "rounded" },
-			---@diagnostic disable-next-line: missing-fields
 			documentation = { border = "rounded" },
 		},
-		experimental = {
-			ghost_text = false,
-		},
-		sorting = defaults.sorting,
+		experimental = { ghost_text = false },
+		sorting = require("cmp.config.default")().sorting,
 	}
 end
 
