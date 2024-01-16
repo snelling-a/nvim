99b6eae099bf44f266b5755df24d64a279b0b428
snelling-a <72226000+snelling-a@users.noreply.github.com>
2024-01-16 10:49:35 +0100

    refactor(lsp): rename
    to work better with mini.files

--- lua/lsp/rename.lua
+++ lua/lsp/rename.lua
@@ -1,53 +1,10 @@
+---@type LSP
+local Lsp = require("lsp")
+local Logger = Lsp.logger
+
 local Methods = vim.lsp.protocol.Methods
 local workspace_willRenameFiles = Methods.workspace_willRenameFiles
 local workspace_didRenameFiles = Methods.workspace_didRenameFiles
-
----@param client lsp.Client
----@param bufnr integer
----@param data {old_name:string,new_name:string}
-local function prepare_rename(client, bufnr, data)
-	local params = {
-		files = {
-			{ newUri = vim.uri_from_fname(data.new_name), oldUri = vim.uri_from_fname(data.old_name) },
-		},
-	}
-
-	---@diagnostic disable-next-line: invisible
-	local resp = client.request_sync(workspace_willRenameFiles, params, 1000, bufnr)
-
-	if resp and resp.result ~= nil then
-		vim.lsp.util.apply_workspace_edit(resp.result, client.offset_encoding)
-	end
-
-	local didRename = vim.tbl_get(client, "server_capabilities", "workspace", "fileOperations", "didRename")
-
-	if didRename ~= nil then
-		client.notify(workspace_didRenameFiles, params)
-	end
-end
-
----@param client lsp.Client
----@param bufnr integer
-local function rename_file(client, bufnr)
-	local old_name = vim.api.nvim_buf_get_name(0)
-
-	vim.ui.input({
-		prompt = "New name: ",
-		default = vim.fn.fnamemodify(old_name, ":t"),
-	}, function(name)
-		if not name then
-			return
-		end
-
-		local new_name = ("%s/%s"):format(vim.fs.dirname(old_name), name)
-		prepare_rename(client, bufnr, {
-			old_name = old_name,
-			new_name = new_name,
-		})
-
-		vim.lsp.util.rename(old_name, new_name, {})
-	end)
-end
 
 local allowed_servers = {
 	lua_ls = true,
@@ -55,12 +12,8 @@ local allowed_servers = {
 	["typescript-tools"] = true,
 }
 
----@class lsp.rename
-local M = {}
-
 ---@param client lsp.Client
----@param bufnr integer
-function M.on_attach(client, bufnr)
+local function is_ok(client)
 	local ok_will, is_will_rename_supported = pcall(function()
 		return client.supports_method(workspace_willRenameFiles)
 	end)
@@ -68,21 +21,44 @@ function M.on_attach(client, bufnr)
 		return client.supports_method(workspace_didRenameFiles)
 	end)
 
-	if
-		not ok_will
-		or not ok_did
-		or not is_will_rename_supported
-		or not is_did_rename_supported
-		or not allowed_servers[client.name]
-	then
-		return
-	end
-
-	local name = client.name:gsub("[-_](%w)", string.upper)
-	name = require("util").capitalize_first_letter(name)
-	vim.api.nvim_create_user_command(("RenameFileWith%s"):format(name), function()
-		rename_file(client, bufnr)
-	end, {})
+	return ok_will or ok_did or is_will_rename_supported or is_did_rename_supported or allowed_servers[client.name]
 end
 
-return M
+---@class lsp.rename
+local M = {}
+
+function M.rename(from, to)
+	local clients = Lsp.util.get_clients()
+	for _, client in pairs(clients) do
+		if is_ok(client) then
+			local params = {
+				files = {
+					{
+						newUri = vim.uri_from_fname(to),
+						oldUri = vim.uri_from_fname(from),
+					},
+				},
+			}
+
+			---@diagnostic disable-next-line: invisible
+			local resp = client.request_sync(workspace_willRenameFiles, params, 1000, 0) or {}
+
+			if resp and resp.result ~= nil then
+				vim.lsp.util.apply_workspace_edit(resp.result, client.offset_encoding)
+				Logger:info("Rename succeeded")
+			end
+
+			local didRename = vim.tbl_get(client, "server_capabilities", "workspace", "fileOperations", "didRename")
+
+			if didRename ~= nil then
+				client.notify(workspace_didRenameFiles, params)
+			end
+		end
+	end
+end
+
+return setmetatable(M, {
+	__call = function(_, ...)
+		return M.rename(...)
+	end,
+})
