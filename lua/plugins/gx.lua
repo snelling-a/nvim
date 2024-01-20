7033ab58825ab8ae17b51e4e9a3b4c818371295e
snelling-a <72226000+snelling-a@users.noreply.github.com>
2024-01-20 22:12:32 +0100

    fix(gx): set keymap
    https://github.com/chrishrb/gx.nvim/commit/eba0eef8143060428c9baf9b14bda6f76b5547b3

--- lua/plugins/gx.lua
+++ lua/plugins/gx.lua
@@ -1,18 +1,26 @@
+local desc = "Better [gx]"
+
 ---@type LazySpec
 local M = { "chrishrb/gx.nvim" }
-
-M.config = true
 
 M.dependencies = { "nvim-lua/plenary.nvim" }
 
 M.keys = {
-	"gx",
-	mode = { "n", "v" },
-	desc = "Better [gx]",
+	{
+		"gx",
+		mode = { "n", "v" },
+		desc = desc,
+	},
 }
 
 M.opts = {
 	handler_options = { search_engine = "ecosia" },
 }
+
+function M.config(_, opts)
+	require("gx").setup(opts)
+
+	require("keymap").map({ "n", "x" }, "gx", vim.cmd.Browse, { desc = desc })
+end
 
 return M
