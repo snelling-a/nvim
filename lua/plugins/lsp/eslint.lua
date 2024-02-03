fb487c2ea2337a417857fd37d2b9b3873d916204
snelling-a <72226000+snelling-a@users.noreply.github.com>
2024-02-03 14:32:14 +0100

    perf: don't use eslint lsp as a formatter
    ever

--- lua/plugins/lsp/eslint.lua
+++ lua/plugins/lsp/eslint.lua
@@ -1,51 +1,19 @@
-local Lsp = require("lsp")
+---@type LazySpec
+local M = { "neovim/nvim-lspconfig" }
 
-return {
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				eslint = {
-					settings = {
-						workingDirectories = { mode = "auto" },
-					},
+M.opts = {
+	servers = {
+		eslint = {
+			settings = {
+				codeAction = {
+					disableRuleComment = { enable = false },
+					showDocumentation = { enable = true },
 				},
-			},
-			setup = {
-				eslint = function()
-					local formatter = require("lsp").format.formatter({
-						name = "eslint: lsp",
-						primary = false,
-						priority = 200,
-						filter = "eslint",
-					})
-
-					if not pcall(require, "vim.lsp._dynamic") then
-						local function get_client(buf)
-							return Lsp.util.get_clients({ name = "eslint", bufnr = buf })[1]
-						end
-
-						formatter.name = "eslint: EslintFixAll"
-						formatter.sources = function(buf)
-							local client = get_client(buf)
-							return client and { "eslint" } or {}
-						end
-						formatter.format = function(buf)
-							local client = get_client(buf)
-							if client then
-								local diag = vim.diagnostic.get(buf, {
-									namespace = vim.lsp.diagnostic.get_namespace(client.id),
-								})
-								if #diag > 0 then
-									vim.cmd.EslintFixAll()
-								end
-							end
-						end
-					end
-
-					Lsp.format.register(formatter)
-				end,
+				format = false,
+				workingDirectories = { mode = "auto" },
 			},
 		},
 	},
 }
+
+return M
