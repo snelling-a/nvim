fa2b10602d218dc4ad92f9a1e68bb69b576007de
snelling-a <72226000+snelling-a@users.noreply.github.com>
2024-02-07 22:37:35 +0100

    perf(leap): remove keymaps
    - get <s>/<S> back for replace
    - flit seems to override it anyway and the s-trigger doesn't really work
    - it is still needed as a dependency of flit

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -5,27 +5,4 @@ M.dependencies = {
 	{ "tpope/vim-repeat", event = require("util").constants.lazy_event },
 }
 
-M.keys = {
-	{
-		"s",
-		mode = { "n", "x", "o" },
-		desc = "Leap forward to",
-	},
-	{
-		"S",
-		mode = { "n", "x", "o" },
-		desc = "Leap backward to",
-	},
-}
-
-function M.config(_, opts)
-	local leap = require("leap")
-	for k, v in pairs(opts) do
-		leap.opts[k] = v
-	end
-	leap.add_default_mappings(true)
-	vim.keymap.del({ "x", "o" }, "x")
-	vim.keymap.del({ "x", "o" }, "X")
-end
-
 return M
