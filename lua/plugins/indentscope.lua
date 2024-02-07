65b59d6327e96af03986de5e4b1d59ccb064a84f
snelling-a <72226000+snelling-a@users.noreply.github.com>
2024-02-07 22:52:06 +0100

    feat(indentscope): remove try_as_border

--- lua/plugins/indentscope.lua
+++ lua/plugins/indentscope.lua
@@ -3,13 +3,11 @@ local M = { "echasnovski/mini.indentscope" }
 
 M.event = require("util").constants.lazy_event
 
-M.opts = {
-	symbol = require("ui.icons").misc.leadmultispace,
-	-- options = { try_as_border = true },
-}
+M.opts = { symbol = require("ui.icons").misc.leadmultispace }
 
 function M.config(_, opts)
 	require("mini.indentscope").setup(opts)
+
 	vim.api.nvim_create_autocmd({ "FileType" }, {
 		callback = function()
 			vim.b.miniindentscope_disable = true
