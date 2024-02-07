f792119e582080cb087ac06ae67f0efc6ecef1a3
snelling-a <72226000+snelling-a@users.noreply.github.com>
2024-02-07 22:26:04 +0100

    perf: move local function into config

--- lua/plugins/git/fugitive.lua
+++ lua/plugins/git/fugitive.lua
@@ -1,33 +1,6 @@
 local Git = vim.cmd.Git
 local desc = "[G]it [s]tatus"
 
----@param ev Ev
-local function set_fugitive_keymaps(ev)
-	if vim.bo.ft ~= "fugitive" then
-		return
-	end
-
-	---@param lhs string
-	---@param rhs function
-	---@param desc string
-	local function leader(lhs, rhs, leader_desc)
-		return require("keymap").leader(lhs, rhs, {
-			buffer = ev.buf,
-			desc = leader_desc,
-		})
-	end
-
-	leader("p", function()
-		Git("push")
-	end, "Git [p]ush")
-	leader("P", function()
-		Git("pull --rebase")
-	end, "Git [P]ull")
-	leader("t", function()
-		Git("push -u origin")
-	end, "Push [t]o origin")
-end
-
 ---@type LazySpec
 local M = { "tpope/vim-fugitive" }
 
@@ -47,7 +20,32 @@ function M.config()
 	end, { desc = desc })
 
 	vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-		callback = set_fugitive_keymaps,
+		---@param ev Ev
+		callback = function(ev)
+			if vim.bo.ft ~= "fugitive" then
+				return
+			end
+
+			---@param lhs string
+			---@param rhs function
+			---@param desc string
+			local function leader(lhs, rhs, leader_desc)
+				return require("keymap").leader(lhs, rhs, {
+					buffer = ev.buf,
+					desc = leader_desc,
+				})
+			end
+
+			leader("p", function()
+				Git("push")
+			end, "Git [p]ush")
+			leader("P", function()
+				Git("pull --rebase")
+			end, "Git [P]ull")
+			leader("t", function()
+				Git("push -u origin")
+			end, "Push [t]o origin")
+		end,
 		desc = "Set fugitive keymaps",
 		group = require("autocmd").augroup("Fugitive"),
 		pattern = "*",
