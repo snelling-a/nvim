45ad988100bf51228900c7e7b88e5bd085c25b28
snelling-a <72226000+snelling-a@users.noreply.github.com>
2024-02-16 09:22:20 +0100

    chore: reimpliment recording after api change
    https://github.com/neovim/neovim/commit/170c890dca8f160bdcd41e4fdc6bf15ee0bdba49

--- lua/ui/shortmess.lua
+++ lua/ui/shortmess.lua
@@ -1,27 +1,27 @@
 local opt = vim.opt
 
 local shortmess_append = {
-	--  l	use "999L, 888B" instead of "999 lines, 888 bytes"
-	--  m	use "[+]" instead of "[Modified]"
-	--  r	use "[RO]" instead of "[readonly]"
-	--  w	use "[w]" instead of "written" for file write message and "[a]" instead of "appended" for ':w >> file' command
-	"a", --  all of the above abbreviations
+	-- l	use "999L, 888B" instead of "999 lines, 888 bytes"
+	-- m	use "[+]" instead of "[Modified]"
+	-- r	use "[RO]" instead of "[readonly]"
+	-- w	use "[w]" instead of "written" for file write message and "[a]" instead of "appended" for ':w >> file' command
+	"a", -- all of the above abbreviations
 	"A", -- don't give the "ATTENTION" message when an existing swap file is found
-	"c", --  don't give ins-completion-menu messages
+	"c", -- don't give ins-completion-menu messages
 	"F", -- don't give the file info when editing a file, like `:silent` was used for the command
-	"I", --  don't give the intro message when starting Vim
+	"I", -- don't give the intro message when starting Vim
 	"o", -- overwrite message for writing a file with subsequent message for reading a file
-	"q", --  use "recording" instead of "recording @a"
-	"s", --  don't give "search hit BOTTOM/TOP, continuing at TOP/BOTTOM" messages; "W" after the count message
+	"s", -- don't give "search hit BOTTOM/TOP, continuing at TOP/BOTTOM" messages; "W" after the count message
 	"t", -- truncate file message at the start if it is too long to fit on the command-line
 	"W", --	don't give "written" or "[w]" when writing a file
 }
 
 local shortmess_remove = {
+	"q", -- do not show "recording @a" when recording a macro
 	"O", -- message for reading a file overwrites any previous message; also for quickfix message (e.g., ":cn")
 	"T", -- Truncate other messages in the middle if they are too long to fit on the command line
-	"C", --  don't give |ins-completion-menu| messages
-	"S", --  do not show search count message when searching, e.g. "[1/5]"
+	"C", -- don't give |ins-completion-menu| messages
+	"S", -- do not show search count message when searching, e.g. "[1/5]"
 }
 
 for _, flag in ipairs(shortmess_append) do
