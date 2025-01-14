082f2b5620072be060ae73c718d76600ccb806d9
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-01-14 15:24:36 +0100

    style: move autocmds

--- init.lua
+++ init.lua
@@ -1,11 +1,4 @@
 require("user")
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,2 +1,3 @@
 require("user.opt")
 require("user.keymap")
+require("user.autocmd")
