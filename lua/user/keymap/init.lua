1e8900dc678826297def181d41b2bd9e6438cbbd
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-03-10 08:36:27 +0100

    refactor: keymap

--- lua/user/keymap/init.lua
+++ lua/user/keymap/init.lua
@@ -1,4 +1,14 @@
-local Maps = require("user.keymap.maps")
+local _set = vim.keymap.set
+
+---@diagnostic disable-next-line: duplicate-set-field
+vim.keymap.set = function(mode, lhs, rhs, opts)
+	opts = opts or {}
+	opts.silent = opts.silent ~= false
+
+	return _set(mode, lhs, rhs, opts)
+end
+
+local Maps = require("user.keymap.util")
 
 Maps.create_breakpoint(")")
 Maps.create_breakpoint(",")
@@ -11,14 +21,8 @@ Maps.create_breakpoint("}")
 Maps.indent("<")
 Maps.indent(">")
 
-Maps.scroll_center(")")
-Maps.scroll_center(")")
-Maps.scroll_center("<C-d")
-Maps.scroll_center("<C-u")
-Maps.scroll_center("G")
-Maps.scroll_center("zx")
-Maps.scroll_center("{")
-Maps.scroll_center("}")
+Maps.scroll_center("<C-d>")
+Maps.scroll_center("<C-u>")
 
 Maps.vertical_move("j")
 Maps.vertical_move("k")
@@ -29,7 +33,7 @@ Maps.wild("p")
 vim.keymap.set(
 	{ "n" },
 	"<leader>s",
-	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/I<Left><Left>]],
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/Ig<Left><Left><Left>]],
 	{ desc = "[S]earch and replace word under the cursor", silent = false }
 )
 
@@ -40,6 +44,11 @@ vim.keymap.set({ "v" }, "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 
 vim.keymap.set({ "n" }, "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
 vim.keymap.set({ "i" }, "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
 
+vim.keymap.set({ "n" }, "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set({ "n" }, "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set({ "n" }, "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set({ "n" }, "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
 vim.keymap.set({ "n" }, "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 vim.keymap.set({ "n" }, "<A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 vim.keymap.set({ "n" }, "<A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
@@ -47,39 +56,13 @@ vim.keymap.set({ "n" }, "<A-Up>", "<cmd>resize +2<cr>", { desc = "Increase Windo
 
 vim.keymap.set({ "n" }, "<C-z>", "<nop>", { desc = "Disable `:stop` keymap" })
 
-vim.keymap.set({ "n" }, "<Esc><Esc>", function()
-	vim.cmd.nohlsearch()
-	vim.cmd.diffupdate()
-	vim.cmd.normal({ args = { "<C-l>" }, bang = true })
-end, { desc = "Redraw / Clear hlsearch / Diff Update" })
+vim.keymap.set({ "n" }, "<Esc>", Maps.clear, { desc = "Redraw / Clear hlsearch / Diff Update" })
 
 vim.keymap.set({ "n" }, "<leader>K", "<cmd>norm! K<cr>", { desc = "Keywordprg" })
 
 vim.keymap.set({ "n", "v" }, "<leader>e", function()
 	vim.cmd.normal({ args = { "%" } })
 end, { desc = "Go to matching bracket" })
-
-vim.keymap.set({ "n" }, "<leader>x", function()
-	local fname = vim.fn.expand("%:p")
-	local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
-
-	if not first_line or not first_line:match("^#!") then
-		local okay, choice =
-			pcall(vim.fn.confirm, ("No shebang found\nmake %q executable?"):format(fname), "&Yes\n&No\n&Cancel")
-		if not okay or choice ~= 1 then
-			vim.notify(("Did not make %q executable"):format(fname), vim.log.levels.WARN)
-			return
-		end
-	end
-
-	local okay, success = pcall(vim.fn.setfperm, fname, "rwxr-xr--")
-	if not okay or success == 0 then
-		vim.notify(("Failed to make %q executable"):format(fname), vim.log.levels.ERROR)
-		return
-	end
-
-	vim.notify(fname .. " is now executable")
-end, { desc = "Make file e[x]ecutable" })
 
 vim.keymap.set({ "n" }, "<leader>l", function()
 	local ok = pcall(vim.cmd.lopen)
@@ -99,61 +82,61 @@ end, { desc = "Quickfix List" })
 
 vim.keymap.set({ "n" }, "J", "mzJ`z", { desc = "Keep cursor while joining lines" })
 
-vim.keymap.set({ "x", "o" }, "N", "'nN'[v:searchforward]", { expr = true, desc = "Visual Prev Search Result" })
-vim.keymap.set({ "n" }, "N", "'nN'[v:searchforward].'zzzv'", { expr = true, desc = "Prev Search Result" })
-vim.keymap.set({ "x", "o" }, "n", "'Nn'[v:searchforward]", { expr = true, desc = "Visual Next Search Result" })
-vim.keymap.set({ "n" }, "n", "'Nn'[v:searchforward].'zzzv'", { expr = true, desc = "Next Search Result" })
+vim.keymap.set(
+	{ "n", "o", "x" },
+	"N",
+	"'nN'[v:searchforward].'zv'",
+	{ expr = true, desc = "Visual Prev Search Result" }
+)
+vim.keymap.set(
+	{ "n", "o", "x" },
+	"n",
+	"'Nn'[v:searchforward].'zv'",
+	{ expr = true, desc = "Visual Next Search Result" }
+)
 
 vim.keymap.set({ "n" }, "U", vim.cmd.redo, { desc = "Redo" })
 
 vim.keymap.set({ "n" }, "[d", Maps.diagnostic_goto(-1), { desc = "Prev Diagnostic" })
-vim.keymap.set({ "n" }, "[e", Maps.diagnostic_goto(-1, vim.diagnostic.severity.ERROR), { desc = "Prev Error" })
-vim.keymap.set({ "n" }, "[w", Maps.diagnostic_goto(-1, vim.diagnostic.severity.WARN), { desc = "Prev Warning" })
 vim.keymap.set({ "n" }, "]d", Maps.diagnostic_goto(1), { desc = "Next Diagnostic" })
+vim.keymap.set({ "n" }, "[e", Maps.diagnostic_goto(-1, vim.diagnostic.severity.ERROR), { desc = "Prev Error" })
 vim.keymap.set({ "n" }, "]e", Maps.diagnostic_goto(1, vim.diagnostic.severity.ERROR), { desc = "Next Error" })
+vim.keymap.set({ "n" }, "[w", Maps.diagnostic_goto(-1, vim.diagnostic.severity.WARN), { desc = "Prev Warning" })
 vim.keymap.set({ "n" }, "]w", Maps.diagnostic_goto(1, vim.diagnostic.severity.WARN), { desc = "Next Warning" })
 
 vim.keymap.set({ "n" }, "<leader>td", function()
-	Config.keymap.toggle.diagnostics()
+	local is_enabled = vim.diagnostic.is_enabled()
+
+	vim.diagnostic.enable(not is_enabled)
 end, { desc = "[T]oggle [D]iagnostics" })
+vim.keymap.set({ "n" }, "<leader>tw", function()
+	local wrap = vim.opt_local.wrap:get()
+
+	vim.opt_local.wrap = not wrap
+end, { desc = "[T]oggle [W]rap" })
 
 vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true })
 vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true })
 
-vim.keymap.set({ "n", "v" }, "z=", function()
-	local word = vim.fn.expand("<cword>")
-	local suggestions = vim.fn.spellsuggest(word)
-
-	vim.ui.select(
-		suggestions,
-		{ prompt = ("Spelling suggestions for %q: "):format(word) },
-		vim.schedule_wrap(function(selected)
-			if selected then
-				vim.cmd.normal({ args = { ("ciw%s"):format(selected) }, bang = true })
-			end
-		end)
-	)
-end)
+-- vim.keymap.set({ "n", "v" }, "z=", function()
+-- 	local word = vim.fn.expand("<cword>")
+-- 	local suggestions = vim.fn.spellsuggest(word)
+--
+-- 	vim.ui.select(
+-- 		suggestions,
+-- 		{ prompt = ("Spelling suggestions for %q: "):format(word) },
+-- 		vim.schedule_wrap(function(selected)
+-- 			if selected then
+-- 				vim.cmd.normal({ args = { ("ciw%s"):format(selected) }, bang = true })
+-- 			end
+-- 		end)
+-- 	)
+-- end)
 
 vim.keymap.set({ "n" }, "<leader>tn", function()
-	local toggle_relative_number = Config.keymap.toggle.option("Relative Number")
-	toggle_relative_number()
-end, { desc = "[T]oggle [N]umber" })
+	local is_enabled = vim.opt_local.relativenumber:get()
 
----@class user.Keymap
----@field maps user.Keymap.maps
----@field toggle user.Keymap.Toggle
----@overload fun(prefix:string):vim.keymap.set.function
-return setmetatable({}, {
-	---@param t user.Keymap
-	---@param prefix? user.Keymap.module_prefix
-	---@return vim.keymap.set.function
-	__call = function(t, prefix)
-		return t.maps.init_mapping(prefix)
-	end,
-	__index = function(t, k)
-		---@diagnostic disable-next-line: no-unknown
-		t[k] = require("user.keymap." .. k)
-		return t[k]
-	end,
-})
+	vim.opt_local.relativenumber = not is_enabled
+end, { desc = "[T]oggle Relative[n]umber" })
+
+vim.keymap.set({ "c" }, "<C-y>", "<c-b>let @*='<c-e>'<cr>", { desc = "Copy contents of the command line" })
--- lua/user/keymap/util.lua
+++ lua/user/keymap/util.lua
@@ -1,5 +1,84 @@
 local M = {}
 
+-- Redraw / Clear hlsearch / Diff Update
+function M.clear()
+	vim.cmd.nohlsearch()
+	vim.cmd.diffupdate()
+	vim.cmd.normal({ args = { "<C-l>" }, bang = true })
+end
+
+--- wrapper for |nvim_feedkeys| that handles <key> syntax
+---@param keys string keys to be typed
+---@param mode? string behavior flags, see `feedkeys()`
+---@param escape_ks? boolean default: true
+---@return nil
+function M.feedkeys(keys, mode, escape_ks)
+	escape_ks = escape_ks ~= false
+
+	if keys:sub(1, 1) == "<" then
+		keys = vim.api.nvim_replace_termcodes(keys, true, false, true)
+		escape_ks = false
+	end
+
+	return vim.api.nvim_feedkeys(keys, mode or "n", escape_ks)
+end
+
+---@type {string:fun():boolean?}[]
+local cmp_actions = {
+	snippet_forward = function()
+		if vim.snippet.active({ direction = 1 }) then
+			vim.schedule(function()
+				vim.snippet.jump(1)
+			end)
+			return true
+		end
+	end,
+	copilot_accept = function()
+		if require("copilot.suggestion").is_visible() then
+			if vim.api.nvim_get_mode().mode == "i" then
+				local create_undo = vim.api.nvim_replace_termcodes("<c-G>u", true, true, true)
+				M.feedkeys(create_undo, "n", true)
+			end
+			require("copilot.suggestion").accept()
+			return true
+		end
+	end,
+}
+
+---@param actions string[]
+---@return fun():boolean|string?
+function M.complete(actions)
+	return function()
+		for _, name in ipairs(actions) do
+			if cmp_actions[name] then
+				local action = cmp_actions[name]()
+				if action then
+					return true
+				end
+			end
+		end
+	end
+end
+
+---@param key ","|"."|";"|")"|"}"|">"|"]" Key to create a break point
+function M.create_breakpoint(key)
+	vim.keymap.set({ "i" }, key, key .. "<c-g>u", { desc = "Add undoable break point at '" .. key .. "'" })
+end
+
+---@param count 1|-1 The number of diagnostics to jump
+---@param severity? vim.diagnostic.Severity See |diagnostic-severity|.
+---@return fun():vim.Diagnostic?
+function M.diagnostic_goto(count, severity)
+	return function()
+		vim.diagnostic.jump({ count = count, severity = severity })
+	end
+end
+
+---@param lhs "<"|">" Left-hand side |{lhs}| of the mapping.
+function M.indent(lhs)
+	vim.keymap.set("x", lhs, lhs .. "gv", { desc = "Keep visually selected area when indenting" })
+end
+
 -- prepends given module name to the description of the keymap
 ---@param str string module name
 ---@return fun(mode: string, lhs: string, rhs: string|function, opts?: vim.keymap.set.Opts) vim.keymap.set
@@ -14,6 +93,59 @@ function M.map(str)
 
 		vim.keymap.set(mode, lhs, rhs, opts)
 	end
+end
+
+function M.quit(bufnr)
+	vim.bo[bufnr].buflisted = false
+	vim.keymap.set({ "n" }, "q", function()
+		xpcall(vim.cmd.close, function()
+			vim.cmd.bwipeout()
+		end)
+
+		vim.cmd.wincmd("p")
+	end, { buffer = bufnr, desc = "Quit Buffer" })
+end
+
+--- Wrapper for |zz| (to scroll center) and |zv| (to open fold) keymaps
+---@param rhs string Right-hand side |{rhs}| of the mapping
+function M.scroll_center(rhs)
+	vim.keymap.set("n", rhs, function()
+		M.feedkeys(rhs)
+
+		M.scroll_unfold()
+	end, { desc = "Center the screen after " .. rhs })
+end
+
+function M.scroll_unfold()
+	M.feedkeys("zzzv")
+end
+
+---@param direction "k"|"j" upwards or downwards
+function M.vertical_move(direction)
+	vim.keymap.set("n", direction, function()
+		if vim.v.count > 0 and vim.v.count >= 3 then
+			return "m'" .. vim.v.count .. direction
+		else
+			return "g" .. direction
+		end
+	end, {
+		desc = "Move to " .. direction == "j" and "next" or "previous" .. " visual line",
+		expr = true,
+		noremap = true,
+	})
+end
+
+---@param lhs "n"|"p" Left-hand side |{lhs}| of the mapping
+function M.wild(lhs)
+	local rhs = lhs == "n" and "down" or "up"
+
+	vim.keymap.set("c", ("<C-%s>"):format(lhs), function()
+		if vim.fn.wildmenumode() == 1 then
+			return lhs
+		else
+			return ("<%s>"):format(rhs)
+		end
+	end, { desc = ("Navigate %s in wildmenu"):format(rhs), expr = true, silent = false })
 end
 
 return M
