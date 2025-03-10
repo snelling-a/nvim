c7a6a12530171e8262a7f75f65e95e3a6411c2e1
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-03-10 08:37:37 +0100

    refactor: lazy init

--- init.lua
+++ init.lua
@@ -14,12 +14,37 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local okay, Event = pcall(require, "lazy.core.handler.event")
+
 if not okay then
-	vim.notify("Lazy.nvim: " .. Event, vim.log.levels.ERROR)
+	vim.notify("Lazy.nvim: " .. vim.inspect(Event), vim.log.levels.ERROR)
 end
+
 Event.mappings.LazyFile = { id = "LazyFile", event = { "BufReadPost", "BufNewFile", "BufWritePre" } }
 Event.mappings["User LazyFile"] = Event.mappings.LazyFile
 
 require("lazy").setup({
-	{ import = "plugins" },
+	change_detection = {
+		notify = false,
+	},
+	---@diagnostic disable-next-line: assign-type-mismatch
+	dev = { path = "~/dev/github.com/snelling-a/" },
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+				-- "matchit",
+				-- "matchparen",
+			},
+			reset = false,
+		},
+	},
+	spec = {
+		{ import = "plugins" },
+	},
+	ui = { border = "rounded" },
 })
