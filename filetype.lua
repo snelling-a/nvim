d0a2ee023641979aa9eacf7a25c409066ae73b12
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-03-21 19:45:08 +0100

    feat: filetype

---
+++ filetype.lua
@@ -0,0 +1,23 @@
+vim.filetype.add({
+	pattern = {
+		["%.?ignore.*"] = "gitignore",
+		["%.env%.[%w_.-]+"] = "sh",
+		[".*%.log"] = "log",
+		["*.pcss"] = "css",
+		[".*/%.?git/attributes"] = "gitattributes",
+		[".*/%.?git/ignore"] = "gitignore",
+		[".*/kitty/.+%.conf"] = "bash",
+		[".*/tmux/.+%.conf"] = "tmux",
+		["known_hosts"] = "sshknownhosts",
+		[".*/%.github/.*%.y*ml"] = "yaml.github",
+		["compose.y.?ml"] = "yaml.docker-compose",
+		["docker%-compose%.y.?ml"] = "yaml.docker-compose",
+		[".*%.?conf(ig)"] = function(path)
+			if path:match("git") then
+				return "gitconfig"
+			end
+
+			return "conf"
+		end,
+	},
+})
