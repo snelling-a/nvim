397861269ba2800e867e0fdbbb4b0b43014e7eaf
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-03-24 08:34:51 +0100

    style(lsp): consistent keymap

--- lua/user/lsp/keymap.lua
+++ lua/user/lsp/keymap.lua
@@ -10,30 +10,30 @@ function M.on_attach(client, bufnr)
 			vim.lsp.inlay_hint.enable(true, { buffer = bufnr })
 		end
 
-		map("n", "<leader>th", function()
+		map({ "n" }, "<leader>th", function()
 			vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ buffer = bufnr }))
 		end, { desc = "[T]oggle Inlay [H]ints" })
 	end
 
 	if client:supports_method(vim.lsp.protocol.Methods.textDocument_declaration, bufnr) then
-		map("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "[G]oto [D]eclaration" })
+		map({ "n" }, "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "[G]oto [D]eclaration" })
 	end
 
 	if client:supports_method(vim.lsp.protocol.Methods.textDocument_definition, bufnr) then
-		map("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "[G]oto [D]efinition" })
+		map({ "n" }, "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "[G]oto [D]efinition" })
 	end
 
 	if client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, bufnr) then
 		local words = require("user.lsp.words")
 
-		map("n", "]]", function()
+		map({ "n" }, "]]", function()
 			if words.get_is_enabled(bufnr) then
 				words.jump(vim.v.count1)
 			else
 				vim.notify("LspWords is not enabled")
 			end
 		end, { buffer = bufnr, desc = "Next Reference" })
-		map("n", "[[", function()
+		map({ "n" }, "[[", function()
 			if words.get_is_enabled(bufnr) then
 				words.jump(-vim.v.count1)
 			else
