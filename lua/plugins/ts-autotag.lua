2c9d104c20140f4dc470958eeb93bae6eeacd8ef
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-03-27 22:16:47 +0100

    perf: lazy load autotag

--- lua/plugins/ts-autotag.lua
+++ lua/plugins/ts-autotag.lua
@@ -1,6 +1,8 @@
 ---@type LazySpec
 return {
 	"windwp/nvim-ts-autotag",
+	event = { "InsertEnter" },
+	ft = { "html", "javascript", "jsx", "markdown", "tsx", "typescript" },
 	config = function()
 		require("nvim-ts-autotag").setup()
 	end,
