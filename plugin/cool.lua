255d5197110e8ccb76439d0201fc1c965db05cf6
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-03-29 20:53:09 +0100

    perf: refactor cool

--- plugin/cool.lua
+++ plugin/cool.lua
@@ -1,36 +1,25 @@
-require("user.autocmd").multi_autocmd_group("cool", function(group)
-	vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-		group = group,
-		callback = function()
-			vim.schedule(function()
-				vim.cmd("nohlsearch")
-			end)
-		end,
-	})
-	vim.api.nvim_create_autocmd("CursorMoved", {
-		group = group,
-		callback = function()
-			local view, rpos = vim.fn.winsaveview(), vim.fn.getpos(".")
-			vim.cmd(
-				string.format(
-					"silent! keepjumps go%s",
-					(vim.fn.line2byte(view.lnum) + view.col + 1 - (vim.v.searchforward == 1 and 2 or 0))
-				)
-			)
+if vim.g.cool_loaded then
+	return
+end
+vim.g.cool_loaded = true
 
-			---@diagnostic disable-next-line: param-type-mismatch
-			local ok, _ = pcall(vim.cmd, "silent! keepjumps norm! n") ---@type boolean, any
-			local insearch = ok
-				and (function()
-					local npos = vim.fn.getpos(".")
-					return npos[2] == rpos[2] and npos[3] == rpos[3]
-				end)()
-			vim.fn.winrestview(view)
-			if not insearch then
-				vim.schedule(function()
-					vim.cmd("nohlsearch")
-				end)
-			end
-		end,
-	})
-end)
+local group = vim.api.nvim_create_augroup("user.cool", {})
+
+vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+	group = group,
+	callback = function()
+		vim.schedule(function()
+			vim.cmd("nohlsearch")
+		end)
+	end,
+})
+vim.api.nvim_create_autocmd("CursorMoved", {
+	group = group,
+	callback = function()
+		if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
+			vim.schedule(function()
+				vim.cmd.nohlsearch()
+			end)
+		end
+	end,
+})
