9b6b5a443f870445700938dfd59bfcb72855bd21
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-05-04 21:00:14 +0200

    feat(typescript): better type hover

---
+++ lua/plugins/better-type-hover.lua
@@ -0,0 +1,8 @@
+---@type LazySpec
+return {
+	"Sebastian-Nielsen/better-type-hover",
+	ft = { "typescript", "typescriptreact" },
+	config = function()
+		require("better-type-hover").setup()
+	end,
+}
