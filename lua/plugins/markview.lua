6719c3a4ea518e211413e9c411f5dff830629623
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-05-08 09:22:14 +0200

    perf(markview): removed dupliated setup call

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -5,16 +5,12 @@ return {
 	enabled = vim.fn.getcwd() ~= vim.fn.expand("~") .. "/notes",
 	ft = { "markdown" },
 	config = function()
-		require("markview").setup()
 		require("markview.extras.headings").setup()
 		require("markview.extras.editor").setup()
 
 		local presets = require("markview.presets")
 		require("markview").setup({
-			markdown = {
-				-- headings = presets.headings.simple,
-				tables = presets.tables.rounded,
-			},
+			markdown = { tables = presets.tables.rounded },
 		})
 	end,
 }
