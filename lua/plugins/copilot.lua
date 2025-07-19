d5d71cf7281761d770e399a38d7b4412d631d85e
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-07-19 16:59:59 +0200

    perf(copilot): no telemetry

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,14 +4,14 @@ return {
 	cmd = { "Copilot" },
 	build = ":Copilot auth",
 	event = { "InsertEnter" },
+	---@type copilot_settings
 	opts = {
 		filetypes = { markdown = true },
 		panel = { enabled = false },
 		suggestion = {
 			auto_trigger = true,
-			keymap = {
-				accept = "<tab>",
-			},
+			keymap = { accept = "<tab>" },
 		},
+		telemetry = { telemetryLevel = "off" },
 	},
 }
