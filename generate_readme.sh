02ea1d1db50f4bd4732dfea416a8189f4ee70f9c
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-09-01 05:48:59 +0000

    chore: update plugins

--- generate_readme.sh
+++ generate_readme.sh
@@ -4,11 +4,11 @@ output="./doc/README.md"
 packages_list='packages.temp'
 
 packages="$(
-	rg '.*("([a-zA-Z0-9-_]+/[.a-zA-Z0-9-_]+)").*' \
-		--glob='lua/plugins/*.lua' \
-		--replace='$2' \
-		--no-line-number \
-		--no-filename | sort -uf
+  rg '.*("([a-zA-Z0-9-_]+/[.a-zA-Z0-9-_]+)").*' \
+    --glob='lua/plugins/*.lua' \
+    --replace='$2' \
+    --no-line-number \
+    --no-filename | sort -uf
 )"
 
 sed -i '/| \[/d' "$output"
@@ -18,14 +18,14 @@ printf "\
       =====  this might take a while  =====\n"
 
 while LF='' read -r line; do
-	description="$(gh repo view "$line" --json description --jq .description)"
-	printf "| [%s](https://github.com/%s) | %s |\n" "$line" "$line" "$description" >>"$packages_list"
+  description="$(gh repo view "$line" --json description --jq .description)"
+  printf "| [%s](https://github.com/%s) | %s |\n" "$line" "$line" "$description" >>"$packages_list"
 
-	gh api \
-		--method PUT \
-		-H "Accept: application/vnd.github+json" \
-		-H "X-GitHub-Api-Version: 2022-11-28" \
-		"/user/starred/$line"
+  gh api \
+    --method PUT \
+    -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "/user/starred/$line"
 done <<<"$packages"
 
 sed -i "/^| -/ r $packages_list" "$output"
