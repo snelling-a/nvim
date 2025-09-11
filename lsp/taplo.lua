789315280d62a6ec0682aadf0390792bc7a721d2
Andrew Snelling <andrewsnelling@Mac.fritz.box>
2025-09-11 18:40:18 +0000

    feat(lsp): taplo

--- lsp/taplo.lua
+++ lsp/taplo.lua
@@ -3,4 +3,17 @@ return {
 	cmd = { "taplo", "lsp", "stdio" },
 	filetypes = { "toml" },
 	single_file_support = true,
+	settings = {
+		taplo = {
+			configFile = { enabled = true },
+			schema = {
+				enabled = true,
+				catalogs = { "https://www.schemastore.org/api/json/catalog.json" },
+				cache = {
+					memoryExpiration = 60,
+					diskExpiration = 600,
+				},
+			},
+		},
+	},
 }
