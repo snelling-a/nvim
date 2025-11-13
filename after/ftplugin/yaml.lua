e20a9984178d1381d8204befd90f63ac2aaf79bc
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-11-13 16:43:46 +0100

    style: formatting

--- after/ftplugin/yaml.lua
+++ after/ftplugin/yaml.lua
@@ -3,6 +3,4 @@ vim.opt_local.cursorcolumn = true
 
 vim.api.nvim_buf_create_user_command(vim.api.nvim_get_current_buf(), "SortYAML", function()
 	vim.cmd([[%!yq 'sort_keys(..)' %]])
-end, {
-	desc = "Sort YAML with `yq`",
-})
+end, { desc = "Sort YAML with `yq`" })
