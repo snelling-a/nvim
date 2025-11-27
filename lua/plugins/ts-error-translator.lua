719d5731c7204f7b630a429d6b00e96773bfa26a
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-11-27 15:42:08 +0100

    chore(ts-error): remove deprecated option

--- lua/plugins/ts-error-translator.lua
+++ lua/plugins/ts-error-translator.lua
@@ -2,5 +2,5 @@
 return {
 	"dmmulroy/ts-error-translator.nvim",
 	ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-	opts = { auto_override_publish_diagnostics = true },
+	opts = {},
 }
