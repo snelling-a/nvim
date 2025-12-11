cde3f006e7c17ef09aeaca878e9037ac96621d95
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-12-11 11:33:38 +0100

    help ft

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -1,4 +1,8 @@
-vim.pack.add({ "https://github.com/OXY2DEV/helpview.nvim" })
-vim.cmd.wincmd("L")
-vim.cmd.resize({ "84", mods = { vertical = true } })
+vim.cmd.packadd("helpview.nvim")
+
+-- Skip window rearrangement for floating windows (fzf-lua preview, etc.)
+if vim.api.nvim_win_get_config(0).relative == "" then
+	vim.cmd.wincmd("L")
+	vim.cmd.resize({ "84", mods = { vertical = true } })
+end
 vim.opt_local.colorcolumn = ""
