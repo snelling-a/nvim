8a1335299f5be0960dcab9cc477cdf2be2f9af18
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-12-18 15:59:31 +0100

    disable markdown plugins in vscode

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,4 +1,6 @@
-vim.cmd.packadd("markdown-preview.nvim")
-vim.cmd.packadd("markdown-plus.nvim")
-require("markdown-plus").setup()
-vim.cmd.packadd("markview.nvim")
+if not vim.g.vscode then
+	vim.cmd.packadd("markdown-preview.nvim")
+	vim.cmd.packadd("markdown-plus.nvim")
+	require("markdown-plus").setup()
+	vim.cmd.packadd("markview.nvim")
+end
