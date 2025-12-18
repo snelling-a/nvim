efe8e0ac97ea57d515447966c5a8fe0cfd83d3fa
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-12-18 16:01:57 +0100

    build markdown preview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,6 +5,22 @@ vim.pack.add({
 	{ src = "https://github.com/YousefHadder/markdown-plus.nvim" },
 }, { load = false })
 
+vim.api.nvim_create_autocmd({ "PackChanged" }, {
+	desc = "Build Markdown Preview",
+	group = vim.api.nvim_create_augroup("user.plugins", { clear = true }),
+	callback = function(event)
+		if event.data.kind == "install" and event.data.spec.name == "markdown-preview.nvim" then
+			vim.notify("Building markdown-preview.nvim...", vim.log.levels.INFO)
+			local ok = pcall(vim.fn["mkdp#util#install"])
+			if ok then
+				vim.notify("markdown-preview.nvim built successfully!")
+			else
+				vim.notify("Error building markdown-preview.nvim: ", vim.log.levels.ERROR)
+			end
+		end
+	end,
+})
+
 local plugins_path = debug.getinfo(1, "S").source:sub(2):match("(.*/)")
 
 for _, file in ipairs(vim.fn.glob(plugins_path .. "*.lua", false, true)) do
