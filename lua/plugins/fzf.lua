0ecd4b550bed2bebc16c68dcdf11e48785507a28
snelling-a <72226000+snelling-a@users.noreply.github.com>
2026-01-05 10:19:35 +0100

    autoload fzflua

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,35 +1,33 @@
-vim.pack.add({ "https://github.com/ibhagwan/fzf-lua" }, {
-	load = function()
-		vim.cmd.packadd("fzf-lua")
-		local actions = require("fzf-lua.actions")
-		require("fzf-lua").setup({
-			"max-perf",
-			files = {
-				hidden = true,
-				cmd = "fd --color=never --type f --hidden --follow --exclude .git && fd --color=never --type f --hidden --no-ignore-vcs --glob '.env*' --exclude .git",
-			},
-			grep = {
-				hidden = true,
-			},
-			lsp = {
-				symbols = { symbol_style = 3 },
-				code_actions = { previewer = false },
-			},
-			keymap = {
-				fzf = { true, ["ctrl-q"] = "select-all+accept" },
-			},
-			helptags = {
-				actions = { ["enter"] = actions.help_vert },
-			},
-		})
+vim.pack.add({ { src = "https://github.com/ibhagwan/fzf-lua" } })
 
-		FzfLua.register_ui_select()
-
-		vim.keymap.set({ "n" }, "<leader>ff", FzfLua.files, { desc = "FzfLua files" })
-		vim.keymap.set({ "n" }, "<leader>fg", FzfLua.live_grep, { desc = "FzfLua live grep" })
-		vim.keymap.set({ "n" }, "<leader>fh", FzfLua.helptags, { desc = "FzfLua help" })
-		vim.keymap.set({ "n" }, "<leader>fk", FzfLua.keymaps, { desc = "FzfLua keymaps" })
-		vim.keymap.set({ "n" }, "<leader>fw", FzfLua.grep_cword, { desc = "FzfLua grep for current word" })
-		vim.keymap.set({ "n" }, "<leader>.", FzfLua.resume, { desc = "FzfLua resume" })
-	end,
+local actions = require("fzf-lua.actions")
+require("fzf-lua").setup({
+	"max-perf",
+	files = {
+		hidden = true,
+		cmd = "fd --color=never --type f --hidden --follow --exclude .git && fd --color=never --type f --hidden --no-ignore-vcs --glob '.env*' --exclude .git",
+	},
+	grep = {
+		hidden = true,
+	},
+	lsp = {
+		symbols = { symbol_style = 3 },
+		code_actions = { previewer = false },
+	},
+	keymap = {
+		fzf = { true, ["ctrl-q"] = "select-all+accept" },
+	},
+	helptags = {
+		actions = { ["enter"] = actions.help_vert },
+	},
 })
+
+FzfLua.register_ui_select()
+
+vim.keymap.set({ "n" }, "<leader>ff", FzfLua.files, { desc = "FzfLua files" })
+vim.keymap.set({ "n" }, "<leader>fb", FzfLua.buffers, { desc = "FzfLua buffers" })
+vim.keymap.set({ "n" }, "<leader>fg", FzfLua.live_grep, { desc = "FzfLua live grep" })
+vim.keymap.set({ "n" }, "<leader>fh", FzfLua.helptags, { desc = "FzfLua help" })
+vim.keymap.set({ "n" }, "<leader>fk", FzfLua.keymaps, { desc = "FzfLua keymaps" })
+vim.keymap.set({ "n" }, "<leader>fw", FzfLua.grep_cword, { desc = "FzfLua grep for current word" })
+vim.keymap.set({ "n" }, "<leader>.", FzfLua.resume, { desc = "FzfLua resume" })
