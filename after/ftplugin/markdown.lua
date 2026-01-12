2da530fae13513ee7978574f5ec0f0cc65df3301
snelling-a <72226000+snelling-a@users.noreply.github.com>
2026-01-12 16:15:22 +0100

    update to live preview

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,5 +1,4 @@
 if not vim.g.vscode then
-	vim.cmd.packadd("markdown-preview.nvim")
 	vim.cmd.packadd("markdown-plus.nvim")
 	require("markdown-plus").setup()
 	vim.cmd.packadd("markview.nvim")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,25 +1,10 @@
 vim.pack.add({
-	{ src = "https://github.com/iamcco/markdown-preview.nvim" },
 	{ src = "https://github.com/OXY2DEV/helpview.nvim" },
 	{ src = "https://github.com/OXY2DEV/markview.nvim" },
 	{ src = "https://github.com/YousefHadder/markdown-plus.nvim" },
+	{ src = "https://github.com/b0o/SchemaStore.nvim" },
+	{ src = "https://github.com/brianhuster/live-preview.nvim" },
 }, { load = false })
-
-vim.api.nvim_create_autocmd({ "PackChanged" }, {
-	desc = "Build Markdown Preview",
-	group = vim.api.nvim_create_augroup("user.plugins", { clear = true }),
-	callback = function(event)
-		if event.data.kind == "install" and event.data.spec.name == "markdown-preview.nvim" then
-			vim.notify("Building markdown-preview.nvim...", vim.log.levels.INFO)
-			local ok = pcall(vim.fn["mkdp#util#install"])
-			if ok then
-				vim.notify("markdown-preview.nvim built successfully!")
-			else
-				vim.notify("Error building markdown-preview.nvim: ", vim.log.levels.ERROR)
-			end
-		end
-	end,
-})
 
 local plugins_path = debug.getinfo(1, "S").source:sub(2):match("(.*/)")
 
