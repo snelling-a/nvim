28190eba6833e2e8ba56b0c2658a711a6a9a9061
snelling-a <72226000+snelling-a@users.noreply.github.com>
2026-03-30 16:36:39 +0200

    perf(lsp): don't load schemas twice

--- lsp/json-lsp.lua
+++ lsp/json-lsp.lua
@@ -16,9 +16,4 @@ return {
 			validate = { enable = true },
 		},
 	},
-	before_init = function(_, new_config)
-		---@diagnostic disable-next-line: inject-field
-		new_config.settings.json.schemas = new_config.settings.json.schemas or {}
-		vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
-	end,
 }
