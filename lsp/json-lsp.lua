2f0bc92261eec9981f6ddc3edae6856e43fb2137
snelling-a <72226000+snelling-a@users.noreply.github.com>
2026-05-11 06:29:51 +0000

    perf(lsp): disable json-lsp format

--- lsp/json-lsp.lua
+++ lsp/json-lsp.lua
@@ -7,7 +7,7 @@ vim.cmd.packadd("SchemaStore.nvim")
 return {
 	cmd = { "vscode-json-language-server", "--stdio" },
 	filetypes = { "json", "jsonc" },
-	init_options = { provideFormatter = true },
+	init_options = { provideFormatter = false },
 	single_file_support = true,
 	settings = {
 		json = {
