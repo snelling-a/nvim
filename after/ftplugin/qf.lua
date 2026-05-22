040e226b9f0e25e0053e0b228da29914e38ae50b
snelling-a <72226000+snelling-a@users.noreply.github.com>
2026-05-22 07:54:30 +0000

    feat(qf): setlocal nolist

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -1,10 +1,11 @@
+vim.opt_local.buflisted = false
 vim.opt_local.cursorline = true
+vim.opt_local.list = false
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
 vim.opt_local.signcolumn = "no"
-vim.opt_local.wrap = false
 vim.opt_local.spell = false
-vim.opt_local.buflisted = false
+vim.opt_local.wrap = false
 
 -- Disable default qf syntax
 vim.cmd("syntax clear")
