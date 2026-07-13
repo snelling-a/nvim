679759ee2961d15b94769e33a5aa9b3b223f45fd
snelling-a <72226000+snelling-a@users.noreply.github.com>
2026-07-13 08:35:18 +0000

    chore: blink.pairs setup

--- lua/plugins/blink-pairs.lua
+++ lua/plugins/blink-pairs.lua
@@ -3,4 +3,18 @@ vim.pack.add({
 	{ src = "https://github.com/saghen/blink.pairs", version = vim.version.range("0.*") },
 })
 
+vim.api.nvim_create_autocmd({ "PackChanged" }, {
+	callback = function(args)
+		if
+			args.data
+			and args.data.spec.name == "blink.pairs"
+			and (args.data.kind == "install" or args.data.kind == "update")
+		then
+			require("blink.pairs").download():pwait(60000)
+		end
+	end,
+	desc = "Handle blink.pairs updates",
+	group = vim.api.nvim_create_augroup("blink.pairs.update-handler", { clear = true }),
+})
+
 require("blink.pairs").setup({})
