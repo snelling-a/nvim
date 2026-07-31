09cf05a62b7a59dd79fb111c106ded7280a6ddf4
snelling-a <72226000+snelling-a@users.noreply.github.com>
2026-07-31 15:18:47 +0000

    perf: remove redundant user commands

--- lua/commands.lua
+++ lua/commands.lua
@@ -24,26 +24,6 @@ for lhs, rhs in pairs(abbrevs) do
 	vim.cmd.cnoreabbrev(lhs .. " " .. rhs)
 end
 
-local function pack_complete()
-	local plugins = vim.pack.get()
-	---@type string[]
-	local names = {}
-	for _, plugin in pairs(plugins) do
-		names[#names + 1] = plugin.spec.name
-	end
-	table.sort(names)
-	return names
-end
-
-vim.api.nvim_create_user_command("PackUpdate", function(opts)
-	local names = #opts.fargs > 0 and opts.fargs or nil
-	vim.pack.update(names)
-end, { nargs = "*", complete = pack_complete, desc = "Update plugins" })
-
-vim.api.nvim_create_user_command("PackClean", function(opts)
-	vim.pack.del(opts.fargs)
-end, { nargs = "+", complete = pack_complete, desc = "Remove plugin" })
-
 vim.api.nvim_create_user_command("Bw", function(opts)
 	local bufdelete = require("bufdelete")
 	local arg = opts.fargs[1]
